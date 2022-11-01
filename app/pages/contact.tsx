d71a044f8175fb3ad5e4967d29f84b8db4a228d8
Jannik Wibker <jannikwibker@gmail.com>
2022-11-01 22:12:57 +0100

    example: test usage of Header and UserMenu on index page and contact page

--- app/pages/contact.tsx
+++ app/pages/contact.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
+import { Header } from '../components/Header'
 
 const ContactPage: NextPage = () => {
   return (
     <div>
+
+      <Header title="helpwave" navigation={[
+        { text: 'Dashboard', href: '/' },
+        { text: 'Contact', href: '/contact' }
+      ]} />
       not sure how this is going to look, just a placeholder right now
     </div>
   )

e7726b4d53a08e733bab2fed35a845fbf2b370e7
Florian Sabonchi <sabonchi@posteo.de>
2022-12-08 23:32:24 +0100

    feat: add margin on mobile

--- landing-page/components/Footer.tsx
+++ landing-page/components/Footer.tsx
@@ -1,17 +1,14 @@
-import React from 'react'
-
 export const Footer = () => {
   return (
-
     <footer className="p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-900">
       <div className="sm:flex">
         <span className="underline">Our Partners</span>
         <img src="https://muensterhack.de/img/logo_muensterhack.png"
-             className="ml-5 h-8" alt="muensterhack Logo"/>
+             className="mt-5 sm:mt-0 ml-5 h-8" alt="muensterhack Logo"/>
         <img src="https://www.ukm.de/typo3conf/ext/ukm_main/Resources/Public/img/UKM_LOGO_2.jpg"
-             className="ml-5 h-8" alt="ukm Logo"/>
+             className="mt-5 sm:mt-0  ml-5 h-8" alt="ukm Logo"/>
         <img src="https://www.warendorf.de/fileadmin/templates/gfx/logo.svg"
-             className="ml-5 h-8" alt="warendorf Logo" />
+             className="mt-5 sm:mt-0  ml-5 h-8" alt="warendorf Logo" />
       </div>
       <div className="mt-14">
         <span className="underline">Our Team</span>

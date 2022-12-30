1cfe3e42cd7313b076d9cabcd8a61db8869a10a5
Jannik Wibker <jannikwibker@gmail.com>
2022-12-30 21:39:16 +0100

    update usermenu and header a tiny bit

--- app/components/UserMenu.tsx
+++ app/components/UserMenu.tsx
@@ -11,15 +11,15 @@ type UserMenuProps = {
 const UserMenu = ({ user }: UserMenuProps) => {
   // TODO: meaningful menu content
   return (
-    <div className="relative">
+    <div className="relative w-40">
       <Menu<HTMLDivElement> alignment="_r" trigger={(onClick, ref) => (
         <div ref={ref} onClick={onClick} className="flex gap-2 relative items-center group cursor-pointer select-none">
           <ProfilePicture avatarUrl={user.avatarUrl} altText={user.displayName} size="small" />
           <div className="text-sm font-semibold text-slate-700 group-hover:text-indigo-400">{user.displayName}</div>
       </div>
       )}>
-        <Link href="/profile"><MenuItem alignment="right">Profile</MenuItem></Link>
-        <Link href="/settings"><MenuItem alignment="right">Settings</MenuItem></Link>
+        <Link href="/profile"><MenuItem alignment="left">Profile</MenuItem></Link>
+        <Link href="/settings"><MenuItem alignment="left">Settings</MenuItem></Link>
       </Menu>
     </div>
   )
--- app/pages/contact.tsx
+++ app/pages/contact.tsx
@@ -5,10 +5,16 @@ const ContactPage: NextPage = () => {
   return (
     <div>
 
-      <Header title="helpwave" navigation={[
-        { text: 'Dashboard', href: '/' },
-        { text: 'Contact', href: '/contact' }
-      ]} />
+      <Header
+        title="helpwave"
+        navigation={[
+          { text: 'Dashboard', href: '/' },
+          { text: 'Contact', href: '/contact' }
+        ]}
+        actions={[
+          <div key="0" className="w-40" />
+        ]}
+      />
       not sure how this is going to look, just a placeholder right now
     </div>
   )

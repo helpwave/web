9190ce84a6c41ffdf1f49a85a1f8b6fa2ca467e9
use-to <13922835+use-to@users.noreply.github.com>
2023-01-11 01:43:33 +0100

    fix: override viewport meta

--- landing-page/pages/_document.tsx
+++ landing-page/pages/_document.tsx
@@ -6,7 +6,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        </Head>
         <body>
           <Main />
           <NextScript />

aedb423a8bc6a22efcfd52dcd991dea551e1518b
Max Schäfer <max-schaefer-lg@gmx.de>
2023-01-11 21:00:28 +0100

    fix: white background for hubspot form

--- landing-page/components/sections/ContactSection.tsx
+++ landing-page/components/sections/ContactSection.tsx
@@ -8,7 +8,7 @@ const ContactSection = forwardRef<HTMLDivElement>(function ContactSection(_, ref
   return (
     <TitleSection id="contact" ref={ref} title="Contact">
       <div className={tw('flex justify-center')}>
-        <GridBox heading={<h1 className={tw('text-5xl font-space font-bold pl-4 pb-4')}></h1>}>
+        <GridBox childrenSurroundingClassName='bg-white'>
             <ContactForm/>
         </GridBox>
       </div>

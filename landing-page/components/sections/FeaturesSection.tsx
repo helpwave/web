5fce6bc0bb7b6f05ee3542b7f93a1a9f118e7f04
DasProffi <prof.pixel77@gmail.com>
2023-01-20 00:20:51 +0100

    refactor: refactored tw attribute order

--- landing-page/components/sections/FeaturesSection.tsx
+++ landing-page/components/sections/FeaturesSection.tsx
@@ -26,19 +26,19 @@ const FeaturesSection = forwardRef<HTMLDivElement>(function FeaturesSection(_, r
         <h1 className={tw('text-5xl font-space font-bold pb-4')}>Solve real world problems</h1>
         <div className={tw("w-5/12 flex flex-col my-24")}>
           {features.map((value, index) =>
-            <div className={tw(`${index % 2 == 0 ? "self-end mr-8" : "self-start ml-8"} w-9/12 flex flex-col my-8`)}>
+            <div className={tw(`w-9/12 flex flex-col ${index % 2 == 0 ? "self-end mr-8" : "self-start ml-8"} my-8`)}>
               {Feature(value)}
             </div>)}
         </div>
       </Section>
-      <div className={tw("w-6/12 h-full absolute right-0 top-0 rounded-3xl pb-16 pt-32 flex flex-col")}
+      <div className={tw("w-6/12 h-full flex flex-col absolute right-0 top-0 rounded-3xl pb-16 pt-32")}
            style={{background: "radial-gradient(#6F387999 15%, #281c2000 60%)"}}>
-        <div className={tw("w-full h-3/6 mb-8 flex flex-row items-end justify-end")}>
-          <div className={tw("bg-gray-200 rounded-3xl w-3/5 h-4/5 relative")}/>
-          <div className={tw("bg-gray-200 w-3/12 h-full ml-8 mt-4 rounded-l-3xl")}/>
+        <div className={tw("w-full h-3/6 flex flex-row items-end justify-end mb-8")}>
+          <div className={tw("w-3/5 h-4/5 relative rounded-3xl bg-gray-200")}/>
+          <div className={tw("w-3/12 h-full rounded-l-3xl ml-8 mt-4 bg-gray-200")}/>
         </div>
         <div className={tw("w-full h-3/6 flex justify-end")}>
-          <div className={tw("bg-gray-200 w-9/12 h-5/6 rounded-l-3xl")}/>
+          <div className={tw("w-9/12 h-5/6 rounded-l-3xl bg-gray-200 ")}/>
         </div>
       </div>
     </div>

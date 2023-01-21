d0a67af81acef275c1eb2db2c4a7dbe979520a98
Jannik Wibker <jannikwibker@gmail.com>
2023-01-21 19:39:56 +0100

    fix: move app/twind/config.ts to app/twind.config.ts and update colors

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -50,21 +50,21 @@ const Button = ({
     className={tx('py-2 px-4 text-sm font-medium focus:outline-none', className, {
 
       // primary & {accent, accent-secondary, positive, negative, neutral}
-      'text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-indigo-500': variant === 'primary' && color === 'accent',
+      'text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-indigo-500': variant === 'primary' && color === 'accent', // TODO use hw-primary instead of indigo
       'text-white bg-AAA-500    hover:bg-AAA-600    focus:ring-AAA-500': variant === 'primary' && color === 'accent-secondary', // TODO: what could this be?
       'text-white bg-XXX-500    hover:bg-XXX-600    focus:ring-XXX-500': variant === 'primary' && color === 'positive', // TODO: green
       'text-white bg-YYY-500    hover:bg-YYY-600    focus:ring-YYY-500': variant === 'primary' && color === 'negative', // TODO: red
       'text-white bg-ZZZ-500    hover:bg-ZZZ-600    focus:ring-ZZZ-500': variant === 'primary' && color === 'neutral', // TODO: maybe blue or yellow?
 
       // secondary & {accent, accent-secondary, positive, negative, neutral}
-      'text-indigo-500 bg-indigo-200 hover:bg-indigo-300 focus:ring-indigo-200': variant === 'secondary' && color === 'accent',
+      'text-indigo-500 bg-indigo-200 hover:bg-indigo-300 focus:ring-indigo-200': variant === 'secondary' && color === 'accent', // TODO use hw-primary instead of indigo
       'text-AAA-500 bg-AAA-200       hover:bg-AAA-300    focus:ring-AAA-200': variant === 'secondary' && color === 'accent-secondary', // TODO: what could this be?
       'text-green-500 bg-green-200   hover:bg-green-300  focus:ring-green-200': variant === 'secondary' && color === 'positive', // TODO: green
       'text-rose-500 bg-rose-200     hover:bg-rose-300   focus:ring-rose-200': variant === 'secondary' && color === 'negative', // TODO: red
       'text-ZZZ-500 bg-ZZZ-200       hover:bg-ZZZ-300    focus:ring-ZZZ-200': variant === 'secondary' && color === 'neutral', // TODO: maybe blue or yellow?
 
       // tertiary & {accent, accent-secondary, positive, negative, neutral}
-      'text-indigo-500 hover:underline focus:ring-indigo-200': variant === 'tertiary' && color === 'accent',
+      'text-indigo-500 hover:underline focus:ring-indigo-200': variant === 'tertiary' && color === 'accent', // TODO use hw-primary instead of indigo
       'text-AAA-500    hover:underline focus:ring-AAA-200': variant === 'tertiary' && color === 'accent-secondary', // TODO: what could this be?
       'text-green-500  hover:underline focus:ring-green-200': variant === 'tertiary' && color === 'positive', // TODO: green
       'text-rose-500   hover:underline focus:ring-rose-200': variant === 'tertiary' && color === 'negative', // TODO: red
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import { ProvideLanguage } from '../hooks/useLanguage'
 import withNextApp from '../twind/next/app'
-import { config } from '../twind/config'
+import { config } from '../twind.config'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <ProvideLanguage><Component {...pageProps} /></ProvideLanguage>

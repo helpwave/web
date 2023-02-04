3c45a5cb10095f21fe2f2abc6ffbfb2d228c73af
Jannik Wibker <jannikwibker@gmail.com>
2023-02-04 13:10:15 +0100

    feat: create `@helpwave/common` lib
    This is a library which solely exists as a package in the web monorepo,
    it is to be used as follows:
    
    ```json
      "@helpwave/common": "workspace:*"
    ```
    
    ```ts
    import { ... } from '@helpwave/common/<straight up file path in lib/>'
    ```
    
    Make sure to configure any build tools to also transpile (certain)
    dependencies, for nextjs this is done using `next.config.js`:
    
    ```js
    /** @type {import('next').NextConfig} */
    const nextConfig = {
      // ...
      experimental: {
        transpilePackages: ['@helpwave/common'],
      }
    }
    
    module.exports = nextConfig
    ```

--- app/next.config.js
+++ app/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   distDir: 'build',
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    transpilePackages: ['@helpwave/common'],
+  }
 }
 
 module.exports = nextConfig

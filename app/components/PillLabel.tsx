02985689b982f5a7563b6b14e3ca6db6b1e30d31
Felix Thape <felix.thape@gmail.com>
2023-03-07 23:14:38 +0100

    feat: add translation

--- app/components/PillLabel.tsx
+++ app/components/PillLabel.tsx
@@ -1,25 +1,56 @@
 import { tw } from '@helpwave/common/twind/index'
 import type { TaskStateInformation } from '../dataclasses/TaskState'
 import { TaskState } from '../dataclasses/TaskState'
+import type { Languages } from '@helpwave/common/hooks/useLanguage'
+import type { PropsWithLanguage } from '@helpwave/common/hooks/useTranslation'
+import { useTranslation } from '@helpwave/common/hooks/useTranslation'
+import type { FunctionComponent } from 'react'
+
+type PillLabelTranslation = {
+  unscheduled: string,
+  inProgress: string,
+  done: string
+}
+
+const defaultTranslation: Record<Languages, PillLabelTranslation> = {
+  en: {
+    unscheduled: 'unscheduled',
+    inProgress: 'in progress',
+    done: 'done'
+  },
+  de: {
+    unscheduled: 'Nicht Geplant',
+    inProgress: 'In Arbeit',
+    done: 'Fertig'
+  },
+}
 
 export type PillLabelProps = {
   count?: number,
   state?: TaskStateInformation
 }
 
-const PillLabel = ({ count = 0, state = TaskState.unscheduled }: PillLabelProps) => {
-  // TODO Translation
-  return (
-    <div className={tw(`flex flex-row pl-2 pr-3 py-1 rounded-lg justify-between
-     bg-${state.colorLabel}-background text-${state.colorLabel}-text text-sm`)}>
-      <div className={tw(`flex flex-row items-center text-${state.colorLabel}-text`)}>
-        <div className={tw(`rounded-full w-2 h-2 bg-${state.colorLabel}-accent`)} />
-        <div className={tw('w-2')} />
-        {state.text}
+const PillLabel: FunctionComponent<PropsWithLanguage<PillLabelTranslation, PillLabelProps>> =
+  (props) => {
+    const translation = useTranslation(props.language, defaultTranslation)
+    props.state ??= TaskState.unscheduled
+    props.count ??= 0
+    const translationMap: {[key:string]:string} = {
+      unscheduled: translation.unscheduled,
+      inProgress: translation.inProgress,
+      done: translation.done
+    }
+    return (
+      <div className={tw(`flex flex-row pl-2 pr-3 py-1 rounded-lg justify-between
+       bg-${props.state.colorLabel}-background text-${props.state.colorLabel}-text text-sm`)}>
+        <div className={tw(`flex flex-row items-center text-${props.state.colorLabel}-text`)}>
+          <div className={tw(`rounded-full w-2 h-2 bg-${props.state.colorLabel}-accent`)} />
+          <div className={tw('w-2')} />
+          {translationMap[props.state.textID]}
+        </div>
+        {props.count}
       </div>
-      {count}
-    </div>
-  )
-}
+    )
+  }
 
 export { PillLabel }
--- app/dataclasses/TaskState.ts
+++ app/dataclasses/TaskState.ts
@@ -1,7 +1,7 @@
 export const TaskState = {
-  unscheduled: { text: 'unscheduled', colorLabel: 'hw-label-1' },
-  inProgress: { text: 'in progress', colorLabel: 'hw-label-2' },
-  done: { text: 'done', colorLabel: 'hw-label-3' },
+  unscheduled: { textID: 'unscheduled', colorLabel: 'hw-label-1' },
+  inProgress: { textID: 'inProgress', colorLabel: 'hw-label-2' },
+  done: { textID: 'done', colorLabel: 'hw-label-3' },
 } as const
 
-export type TaskStateInformation = { text: string, colorLabel: string }
+export type TaskStateInformation = { textID: string, colorLabel: string }

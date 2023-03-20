08e5394eadd4da98fb1596af00109e9028835f88
Felix Thape <felix.thape@gmail.com>
2023-03-20 23:01:01 +0100

    feat: make callbacks optional with default values

--- app/components/WardTile.tsx
+++ app/components/WardTile.tsx
@@ -31,32 +31,38 @@ type WardDTO = {
 export type WardTileProps = {
   ward: WardDTO,
   isSelected: boolean,
-  tileClickedCallback: () => void,
-  editClickedCallback: () => void
+  tileClickedCallback?: () => void,
+  editClickedCallback?: () => void
 }
 
-const WardTile: FunctionComponent<PropsWithLanguage<WardTileTranslation, WardTileProps>> = (props) => {
-  const translation = useTranslation(props.language, defaultWardTileTranslations)
+const WardTile: FunctionComponent<PropsWithLanguage<WardTileTranslation, WardTileProps>> = ({
+  language,
+  isSelected,
+  ward,
+  tileClickedCallback = () => undefined,
+  editClickedCallback = () => undefined
+}: PropsWithLanguage<WardTileTranslation, WardTileProps>) => {
+  const translation = useTranslation(language, defaultWardTileTranslations)
   // TODO change border color for onHover and selected
-  const selectedBorderColor = props.isSelected ? 'border-black' : ''
+  const selectedBorderColor = isSelected ? 'border-black' : ''
   return (
-    <div onClick={props.tileClickedCallback}
+    <div onClick={tileClickedCallback}
          className={tw(`${selectedBorderColor} group cursor-pointer rounded-md py-2 px-4 border hover:border-black w-full`)}>
       <div className={tw('flex flex-row justify-between w-full')}>
-        <h5 className={tw('font-bold')}>{props.ward.name}</h5>
-        <button onClick={props.editClickedCallback}
+        <h5 className={tw('font-bold')}>{ward.name}</h5>
+        <button onClick={editClickedCallback}
                 className={tw('hidden group-hover:block')}>{translation.edit}</button>
       </div>
-      <div className={tw('text-left my-1')}>{props.ward.roomNames.join(', ')}</div>
+      <div className={tw('text-left my-1')}>{ward.roomNames.join(', ')}</div>
       <div className={tw('flex flex-row justify-between w-full')}>
         <div className={tw('flex flex-row')}>
           <Bed/>
-          <div className={tw('pl-1')}>{props.ward.bedCount}</div>
+          <div className={tw('pl-1')}>{ward.bedCount}</div>
         </div>
         <PillLabelBox
-          unscheduledCount={props.ward.unscheduledTasks}
-          inProgressCount={props.ward.inProgressTasks}
-          doneCount={props.ward.doneTasks}/>
+          unscheduledCount={ward.unscheduledTasks}
+          inProgressCount={ward.inProgressTasks}
+          doneCount={ward.doneTasks}/>
       </div>
     </div>
   )

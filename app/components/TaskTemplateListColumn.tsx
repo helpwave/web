4bf06f7bc24eebb6bfb8480460703e3539764d83
Felix Thape <felix.thape@gmail.com>
2023-03-21 13:28:42 +0100

    feat: add task template column

---
+++ app/components/TaskTemplateListColumn.tsx
@@ -0,0 +1,66 @@
+import { tw } from '@helpwave/common/twind/index'
+import type { Languages } from '@helpwave/common/hooks/useLanguage'
+import type { PropsWithLanguage } from '@helpwave/common/hooks/useTranslation'
+import { useTranslation } from '@helpwave/common/hooks/useTranslation'
+import { useState } from 'react'
+import { TaskTemplateTile } from './TaskTemplateTile'
+import Add from '../icons/Add'
+
+export type TaskTemplateListColumnTranslation = {
+  name: string,
+  addNewTaskTemplate: string
+}
+
+type TaskTemplateDTO = {
+  name: string,
+  subtaskCount: number
+}
+
+export type TaskTemplateListColumnProps = {
+  translations: Record<Languages, TaskTemplateListColumnTranslation>,
+  taskTemplates: TaskTemplateDTO[]
+}
+
+export const TaskTemplateListColumn = ({
+  language,
+  translations,
+  taskTemplates
+}: PropsWithLanguage<TaskTemplateListColumnTranslation, TaskTemplateListColumnProps>) => {
+  const [selected, setSelected] = useState<TaskTemplateDTO | undefined>(undefined)
+  const translation = useTranslation(language, translations)
+
+  // TODO update with opening of the create template screen
+  const addNewTask = () => undefined
+
+  const onTileClicked = (taskTemplate: TaskTemplateDTO) => {
+    // TODO open template edit screen
+    setSelected(taskTemplate)
+  }
+
+  // TODO action on clicking the template tile
+  const onEditClicked = (taskTemplate: TaskTemplateDTO) => taskTemplate
+
+  return (
+    <div>
+      <div className={tw('flex flex-row items-center')}>
+        <div className={tw('w-2 h-2 mx-2 rounded-full bg-gray-300')}/>
+        <span className={tw('font-bold')}>{translation.name}</span>
+      </div>
+      {taskTemplates.map(taskTemplate => (
+          <div key={taskTemplate.name} className={tw('my-2')}>
+            <TaskTemplateTile name={taskTemplate.name}
+                              subtaskCount={taskTemplate.subtaskCount}
+                              isSelected={selected === taskTemplate}
+                              onEditClicked={() => onEditClicked(taskTemplate)}
+                              onTileClicked={() => onTileClicked(taskTemplate)}/>
+          </div>
+      )
+      )}
+      <div onClick={addNewTask}
+           className={tw('flex flex-row w-full h-16 rounded-md border-2 hover:border-hw-primary-700 items-center justify-center cursor-pointer mt-4')}>
+        <Add/>
+        <span className={tw('ml-2')}>{translation.addNewTaskTemplate}</span>
+      </div>
+    </div>
+  )
+}

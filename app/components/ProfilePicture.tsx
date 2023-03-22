3b62db71090ab48e38977bd104ab0581584fb05c
Felix Thape <felix.thape@gmail.com>
2023-03-22 01:36:16 +0100

    feat: remove static color for an image

--- app/components/ProfilePicture.tsx
+++ app/components/ProfilePicture.tsx
@@ -3,13 +3,14 @@ import { tw, tx } from '@helpwave/common/twind/index'
 export type ProfilePictureProps = {
   avatarUrl: string,
   altText: string,
-  size?: 'small' | 'medium' | 'large'
+  size?: 'tiny' | 'small' | 'medium' | 'large'
 }
 
 const ProfilePicture = ({ avatarUrl, altText, size = 'medium' }: ProfilePictureProps) => {
   return (
     <div className={tw('rounded-full')}>
       <img className={tx('rounded-full border border-slate-200 group-hover:border-indigo-200', {
+        'h-6 w-6': size === 'tiny',
         'h-8 w-8': size === 'small',
         'h-12 w-12': size === 'medium',
         'h-16 w-16': size === 'large'
--- app/components/StackedProfiles.tsx
+++ app/components/StackedProfiles.tsx
@@ -2,23 +2,24 @@ import { tw, tx } from '@helpwave/common/twind/index'
 import type { Languages } from '@helpwave/common/hooks/useLanguage'
 import type { PropsWithLanguage } from '@helpwave/common/hooks/useTranslation'
 import { useTranslation } from '@helpwave/common/hooks/useTranslation'
+import { ProfilePicture } from './ProfilePicture'
 
 type StackedProfilesTranslation = {
-  others: (users: number) => string
+  others: (isMultipleUsers: boolean) => string
 }
 
 const defaultStackedProfilesTranslations: Record<Languages, StackedProfilesTranslation> = {
   en: {
-    others: (users: number) => users === 1 ? 'other' : 'others'
+    others: (isMultipleUsers: boolean) => isMultipleUsers ? 'other' : 'others'
   },
   de: {
-    others: (users: number) => users === 1 ? 'weiterer' : 'weitere'
+    others: (isMultipleUsers: boolean) => isMultipleUsers ? 'weiterer' : 'weitere'
   }
 }
 
 // TODO replace later
 type UserDTO = {
-  color: string,
+  avatarURL: string,
   name: string
 }
 
@@ -34,18 +35,24 @@ export const StackedProfiles = ({
 }: PropsWithLanguage<StackedProfilesTranslation, StackedProfilesProps>) => {
   const translation = useTranslation(language, defaultStackedProfilesTranslations)
   const displayedProfiles = users.length < maxShownProfiles ? users : users.slice(0, maxShownProfiles)
-  const height = 24 // 24px
+  const diameter = 24 // 24px
   const stackingOverlap = 0.5 // given as a percentage
-
+  const notDisplayedProfiles = users.length - maxShownProfiles
   return (
-    <div className={tw(`h-[${height + 'px'}] flex flex-row relative`)}>
+    <div className={tw(`h-[${diameter + 'px'}] flex flex-row relative`)}>
       {displayedProfiles.map((user, index) => (
-        <div key={user.name}
-             className={tx(`h-[${height + 'px'}] w-[${height + 'px'}] rounded-full border-1 bg-[${user.color}] absolute left-[${(index * height * stackingOverlap) + 'px'}] z-[${maxShownProfiles - index}]`)}/>
+        <div key={user.name} className={tx(`absolute left-[${(index * diameter * stackingOverlap) + 'px'}] z-[${maxShownProfiles - index}]`)}>
+          <ProfilePicture key={user.name} avatarUrl={user.avatarURL} altText="" size="tiny"/>
+        </div>
       ))}
-      <div className={tx(`ml-[${((maxShownProfiles + 1) * height * stackingOverlap + 4) + 'px'}]`, { hidden: users.length <= maxShownProfiles })}>
-        <span>{`+ ${(users.length - maxShownProfiles)} ${translation.others(users.length - maxShownProfiles)}`}</span>
-      </div>
+      {notDisplayedProfiles <= 0 ? null :
+          (
+          <div
+            className={tx(`ml-[${((maxShownProfiles + 1) * diameter * stackingOverlap + 4) + 'px'}]`)}>
+            <span>{`+ ${(notDisplayedProfiles)} ${translation.others(notDisplayedProfiles > 1)}`}</span>
+          </div>
+          )
+      }
     </div>
   )
 }

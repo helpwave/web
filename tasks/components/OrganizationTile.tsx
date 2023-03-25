a7aeb91ac2c0561a330beb0fd003479adad90478
Felix Thape <felix.thape@gmail.com>
2023-03-25 17:16:05 +0100

    feat: limit number of displayed ward names

--- tasks/components/OrganizationTile.tsx
+++ tasks/components/OrganizationTile.tsx
@@ -8,15 +8,21 @@ import Email from '@helpwave/common/icons/Email'
 import { AvatarGroup } from './AvatarGroup'
 
 type OrganizationTileTranslation = {
-  edit: string
+  edit: string,
+  other: string,
+  others: string
 }
 
 const defaultOrganizationTileTranslations: Record<Languages, OrganizationTileTranslation> = {
   en: {
-    edit: 'Edit'
+    edit: 'Edit',
+    other: 'other',
+    others: 'others'
   },
   de: {
-    edit: 'Bearbeiten'
+    edit: 'Bearbeiten',
+    other: 'weiterer',
+    others: 'othere'
   }
 }
 
@@ -38,18 +44,22 @@ type OrganizationDTO = {
 }
 
 export type OrganizationTileProps = CardProps & {
+  displayedAvatars?: number,
   organization: OrganizationDTO,
   onEditClick?: () => void
 }
 
 export const OrganizationTile = ({
   language,
+  displayedAvatars = 5,
   isSelected,
   organization,
   onTileClick = () => undefined,
   onEditClick = () => undefined
 }: PropsWithLanguage<OrganizationTileTranslation, OrganizationTileProps>) => {
   const translation = useTranslation(language, defaultOrganizationTileTranslations)
+  const notDisplayedWards = Math.max(0, organization.wards.length - displayedAvatars)
+
   return (
     <Card onTileClick={onTileClick} isSelected={isSelected} className={tw('group cursor-pointer')}>
       <div className={tw('flex flex-row justify-between w-full')}>
@@ -60,12 +70,13 @@ export const OrganizationTile = ({
         </button>
       </div>
       <div className={tw('text-left my-1 font-semibold text-gray-600 text-sm')}>
-        {organization.wards.map(value => value.name).join(', ')}
+        {organization.wards.slice(0, displayedAvatars).map(value => value.name).join(', ')}
+        {notDisplayedWards > 0 && (<span className={tw('ml-1')}>{`+ ${notDisplayedWards} ${notDisplayedWards === 1 ? translation.other : translation.others}`}</span>)}
       </div>
       <div className={tw('flex flex-row justify-between w-full')}>
-        <div className={tw('flex flex-row')}>
+        <div className={tw('flex flex-row items-center')}>
           <Email />
-          <div className={tw('ml-2')}>{organization.email}</div>
+          <span className={tw('ml-2 text-sm')}>{organization.email}</span>
         </div>
         <AvatarGroup users={organization.members}/>
       </div>

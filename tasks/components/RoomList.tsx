2a2538fa51975970cf4d8bf1236f8ef72566b09b
Felix Thape <felix.thape@gmail.com>
2023-04-07 16:21:50 +0200

    feat: add room list

---
+++ tasks/components/RoomList.tsx
@@ -0,0 +1,222 @@
+import {
+  useReactTable,
+  createColumnHelper,
+  getCoreRowModel,
+  getPaginationRowModel
+} from '@tanstack/react-table'
+import { tw } from '@helpwave/common/twind/index'
+import type { Languages } from '@helpwave/common/hooks/useLanguage'
+import type { PropsWithLanguage } from '@helpwave/common/hooks/useTranslation'
+import { useTranslation } from '@helpwave/common/hooks/useTranslation'
+import Dropdown from '../icons/TriangleDown'
+import { Pagination } from './Pagination'
+import { TriStateCheckbox } from './user_input/TriStateCheckbox'
+import { Button } from './Button'
+import { Input } from './user_input/Input'
+
+type RoomListTranslation = {
+  edit: string,
+  remove: string,
+  deselectAll: string,
+  selectAll: string,
+  rooms: string,
+  addRoom: string,
+  bedCount: string
+}
+
+const defaultRoomListTranslations: Record<Languages, RoomListTranslation> = {
+  en: {
+    edit: 'Edit',
+    remove: 'Remove',
+    deselectAll: 'Deselect All',
+    selectAll: 'Select All',
+    rooms: 'Rooms',
+    addRoom: 'Add Room',
+    bedCount: 'Bedcount'
+  },
+  de: {
+    edit: 'Bearbeiten',
+    remove: 'Entfernen',
+    deselectAll: 'Auswahl aufheben',
+    selectAll: 'Alle auswählen',
+    rooms: 'Räume',
+    addRoom: 'Raum hinzufügen',
+    bedCount: 'Bettenanzahl'
+  }
+}
+
+type Room = {
+  bedCount: number,
+  name: string
+}
+
+export type RoomListProps = {
+  rooms: Room[],
+  roomsPerPage?: number,
+  onChange: (rooms: Room[]) => void
+}
+
+const columnHelper = createColumnHelper<Room>()
+
+const columns = [
+  columnHelper.display({
+    id: 'select',
+  }),
+  columnHelper.accessor('name', {
+    id: 'name',
+  }),
+  columnHelper.accessor('bedCount', {
+    id: 'bedCount',
+  }),
+  columnHelper.display({
+    id: 'remove',
+  }),
+]
+
+export const RoomList = ({
+  language,
+  roomsPerPage = 5,
+  rooms,
+  onChange
+}: PropsWithLanguage<RoomListTranslation, RoomListProps>) => {
+  const translation = useTranslation(language, defaultRoomListTranslations)
+
+  const table = useReactTable({
+    data: rooms,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    initialState: { pagination: { pageSize: roomsPerPage } }
+  })
+
+  const addRoom = () => {
+    const defaultBedCount = 3
+    // TODO remove below for an actual room add
+    const newRoom = {
+      name: 'room' + (rooms.length + 1),
+      bedCount: defaultBedCount,
+      isSelected: false
+    }
+    onChange([...rooms, newRoom])
+  }
+
+  return (
+    <div className={tw('flex flex-col')}>
+      <div className={tw('flex flex-row justify-between items-center mb-2')}>
+        <span className={tw('font-bold font-space')}>{translation.rooms + ` (${rooms.length})`}</span>
+        <Button onClick={addRoom} color="positive">
+          <div className={tw('flex flex-row items-center')}>
+            <span className={tw('mr-2')}>{translation.addRoom}</span>
+            <Dropdown/>
+          </div>
+        </Button>
+      </div>
+      <table>
+        <thead>
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : {
+                      select:
+                      (<div className={tw('pr-4')}>
+                          <TriStateCheckbox
+                            checked={table.getIsSomePageRowsSelected() ? null : table.getIsAllRowsSelected()}
+                            onChanged={() => table.toggleAllRowsSelected()}
+                          />
+                        </div>
+                      ),
+                      name:
+                      (<div className={tw('flex flex-row')}>
+                        <span>{table.getIsAllRowsSelected() ? translation.deselectAll : translation.selectAll}</span>
+                      </div>),
+                      bedCount:
+                      (<div className={tw('flex flex-row justify-end items-center')}>
+                        {translation.bedCount}
+                      </div>),
+                      remove:
+                      (<div className={tw('flex flex-row justify-end pl-8')}>
+                        <button
+                          onClick={() => {
+                            table.toggleAllRowsSelected(false)
+                            onChange(rooms.filter(value => !table.getSelectedRowModel().rows.find(row => row.original === value)))
+                          }}>
+                          <span>{translation.remove}</span>
+                        </button>
+                      </div>),
+                    }[header.column.id]
+                }
+              </th>
+            ))}
+          </tr>
+        ))}
+        </thead>
+        <tbody>
+        {table.getRowModel().rows.map(row => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map(cell => (
+              <td key={cell.id}>
+                {{
+                  bedCount: (
+                    <div className={tw('w-20')}>
+                      <Input
+                        value={cell.row.original.bedCount.toString()}
+                        type="number"
+                        onChange={(text) => onChange(rooms.map(value => value.name === cell.row.original.name ? {
+                          ...cell.row.original,
+                          bedCount: parseInt(text)
+                        } : value))}
+                        min={1}
+                        id={cell.row.original.bedCount.toString()}
+                      />
+                    </div>
+                  ),
+                  name: (
+                    <div className={tw('flex flex-row items-center w-1/2 min-w-[50px]')}>
+                      <Input
+                        value={cell.row.original.name}
+                        type="text"
+                        onChange={(text) => onChange(rooms.map(value => value.name === cell.row.original.name ? {
+                          ...cell.row.original,
+                          name: text
+                        } : value))}
+                        id={cell.row.original.name}
+                      />
+                    </div>
+                  ),
+                  remove: (
+                    <div className={tw('flex flex-row justify-end')}>
+                      <button onClick={() => onChange(rooms.filter(value => value !== cell.row.original))}>
+                        <span className={tw('text-hw-negative-500')}>{translation.remove}</span>
+                      </button>
+                    </div>
+                  ),
+                  select: <TriStateCheckbox checked={cell.row.getIsSelected()}
+                                            onChanged={() => cell.row.toggleSelected()}/>
+                }[cell.column.id]}
+              </td>
+            ))}
+          </tr>
+        ))}
+        {table.getState().pagination.pageIndex === (table.getPageCount() - 1) && table.getPageCount() > 1
+          && (rooms.length % roomsPerPage) !== 0
+          && ([...Array((roomsPerPage - (rooms.length % roomsPerPage)) % roomsPerPage)].map((i, index) => (
+            <tr key={index} className={tw('h-12')}>
+              {[table.getAllColumns.length].map((j, index) => (
+                <td key={index}/>
+              ))}
+            </tr>
+          )))}
+        </tbody>
+      </table>
+      <div className={tw('flex flex-row justify-center mt-2')}>
+        <Pagination page={table.getState().pagination.pageIndex}
+                    numberOfPages={Math.max(table.getPageCount(), 1)}
+                    onPageChanged={table.setPageIndex}
+        />
+      </div>
+    </div>
+  )
+}
--- tasks/components/user_input/Input.tsx
+++ tasks/components/user_input/Input.tsx
@@ -10,7 +10,7 @@ type InputProps = {
    */
   id: string,
   value: string,
-  label: string,
+  label?: string,
   /**
    * @default 'text'
    */
@@ -27,7 +27,7 @@ type InputProps = {
 const ControlledInput = ({ id, type = 'text', value, label, onChange = noop, ...restProps }: InputProps) => {
   return (
     <div className={tw('w-full')}>
-      <label htmlFor={id} className={tw('block text-sm font-medium text-gray-700')}>{label}</label>
+      {label && <label htmlFor={id} className={tw('block text-sm font-medium text-gray-700')}>{label}</label>}
       <input
         value={value}
         id={id}

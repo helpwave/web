0218cc988eb59e0484a0a2f292362e02bb1d6954
Florian Sabonchi <Sabonchi@posteo.de>
2023-04-08 14:28:52 +0200

    feat: code review
    Co-authored-by: Felix <67233923+DasProffi@users.noreply.github.com>
    
    Co-authored-by: Felix <13922835+use-to@users.noreply.github.com>

--- tasks/components/user_input/Textarea.tsx
+++ tasks/components/user_input/Textarea.tsx
@@ -1,20 +1,30 @@
-import { tw } from '@helpwave/common/twind'
+import { css, tw } from '@helpwave/common/twind'
+import type { TextareaHTMLAttributes } from 'react'
 
 type TextareaProps = {
-  headline : string,
-  placeholder : string,
-  message : string,
-  id : string
+  headline?: string,
+  placeholder?: string,
+  message?: string,
+  id?: string,
+  onChange: (text: string) => void,
+  props?: TextareaHTMLAttributes<HTMLTextAreaElement>
 }
-export const Textarea = ({ placeholder, headline, message, id } : TextareaProps) => {
-  return (
-    <div className={tw('relative shadow border-1 border-gray-300 rounded-lg')}>
-      <label className={tw('mx-3 mt-3  block text-gray-700 font-bold')} htmlFor={id}>
-        {headline}
-      </label>
-      <textarea defaultValue={message} className={tw('pt-0 border-transparent focus:border-transparent focus:ring-0 h-32 resize-none  appearance-none border w-full text-gray-700 leading-tight focus:outline-none')} id={id} placeholder={placeholder}>
 
+const globalStyles = css`
+  .textarea-wrapper:focus-within {
+    @apply border-hw-primary-700;
+  }
+`
+export const Textarea = ({ headline, placeholder, message, id } : TextareaProps) => {
+  return (
+    <div className={tw(globalStyles)}>
+      <div className={`textarea-wrapper ${tw('relative shadow border-1 border-gray-300 rounded-lg')}`}>
+        <label className={tw('mx-3 mt-3 block text-gray-700 font-bold')} htmlFor={id}>
+          {headline}
+        </label>
+        <textarea defaultValue={message} className={tw('pt-0 border-transparent focus:border-transparent focus:ring-0 h-32 resize-none appearance-none border w-full text-gray-700 leading-tight focus:outline-none')} id={id} placeholder={placeholder}>
       </textarea>
+      </div>
     </div>
   )
 }

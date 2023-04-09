32ee9cbcedda60fdf45fb93a0507b3e107f7c5f4
Florian Sabonchi <Sabonchi@posteo.de>
2023-04-09 10:58:45 +0200

    feat: Updated formatting
    Co-authored-by: Felix <67233923+DasProffi@users.noreply.github.com>

--- tasks/components/user_input/Textarea.tsx
+++ tasks/components/user_input/Textarea.tsx
@@ -32,7 +32,12 @@ export const Textarea = ({ headline, placeholder, message, id } : TextareaProps)
         <label className={tw('mx-3 mt-3 block text-gray-700 font-bold')} htmlFor={id}>
           {headline}
         </label>
-        <textarea defaultValue={message} className={tw('pt-0 border-transparent focus:border-transparent focus:ring-0 h-32 resize-none appearance-none border w-full text-gray-700 leading-tight focus:outline-none')} id={id} placeholder={placeholder}>
+        <textarea 
+          defaultValue={message}
+          className={tw('pt-0 border-transparent focus:border-transparent focus:ring-0 h-32 resize-none appearance-none border w-full text-gray-700 leading-tight focus:outline-none')}
+          id={id}
+          placeholder={placeholder}
+        >
       </textarea>
       </div>
     </div>

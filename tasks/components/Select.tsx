268fecafa99f54e2a8b81644f0c69ddab311b028
aegis301 <57251443+aegis301@users.noreply.github.com>
2023-04-12 12:18:03 +0200

    wip: styling menu items

--- tasks/components/Select.tsx
+++ tasks/components/Select.tsx
@@ -1,39 +1,69 @@
 import { tw } from '@helpwave/common/twind/index'
 import { useState } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+
+type Option = {
+  label: string,
+  value: string
+}
 
 type SelectProps = {
+  value: string,
   label: string,
-  options: string[],
+  options: Option[],
   onChange: (value: string) => void
 };
 
-export const Select = ({ label, value, onChange, options }: SelectProps) => {
-  const [selected, setSelected] = useState<string>(value)
-  const [open, setOpen] = useState<boolean>(false)
+export const Select = ({ label, onChange, options }: SelectProps) => {
+  const [selected, setSelected] = useState<string>(options[0].value)
 
-  const handleChange = (value: string) => {
-    setSelected(value)
+  const handleChange = (value: string, label: string) => {
+    setSelected(label)
     onChange(value)
   };
 
   return (
-    <div className={tw('flex flex-col mx-2')}>
-      <label className={tw('text-base font-bold mb-1 text-slate-700')}>
+    <div className={tw('relative')}>
+      <label htmlFor={label} className={tw('block font-semibold mb-1')}>
         {label}
       </label>
-      <select
-        className={tw(
-          'mt-1 block rounded-md w-full border-gray-300 shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-indigo-500'
+      <Menu as="div" className={tw('relative inline-block text-left')}>
+        {({ open }) => (
+          <>
+          <Menu.Button className={tw('inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500')}>
+            <span>{selected}</span>
+            <ChevronDownIcon className={tw('-mr-1 ml-2 h-5 w-5')} aria-hidden="true" />
+          </Menu.Button>
+          <Transition
+              show={open}
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+            >
+          <Menu.Items className={tw('absolute z-10 mt-1 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none')}>
+          {options.map(option => (
+                  <Menu.Item key={option.value}>
+                    {({ active }) => (
+                      <div
+                        className={tw(`px-4 py-2 cursor-pointer hover:bg-gray-100 ${
+                          option.value === selected ? 'bg-gray-200' : ''
+                        }`)}
+                        onClick={() => handleChange(option.value, option.label)}
+                      >
+                        {option.label}
+                      </div>
+                    )}
+                  </Menu.Item>
+          ))}
+              </Menu.Items>
+              </Transition>
+          </>
         )}
-        value={selected}
-        onChange={(e) => handleChange(e.target.value)}
-      >
-        {options.map((option) => (
-          <option key={option} value={option} className={tw('text-slate-700')}>
-            {option}
-          </option>
-        ))}
-      </select>
-    </div>
+        </Menu>
+      </div>
   );
 };

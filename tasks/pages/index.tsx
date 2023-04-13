06ac469eed3ca12018d4ccff823e929cbfac217a
use-to <13922835+use-to@users.noreply.github.com>
2023-04-13 14:54:07 +0200

    chore: revert changes made in

--- tasks/pages/index.tsx
+++ tasks/pages/index.tsx
@@ -3,27 +3,21 @@ import Head from 'next/head'
 import { tw } from '@helpwave/common/twind/index'
 import { useAuth } from '../hooks/useAuth'
 import { useRouter } from 'next/router'
-import { Header } from '../components/Header'
-import { UserMenu } from '../components/UserMenu'
 import { WardServiceClient } from '../generated/Ward_svcServiceClientPb'
 import { CreateWardRequest, GetWardRequest } from '../generated/ward_svc_pb'
+import { PageWithHeader } from '../components/layout/PageWithHeader'
 
 const Home: NextPage = () => {
   const router = useRouter()
-  const { user, logout, accessToken } = useAuth(() =>
-    router.push({ pathname: '/login', query: { back: true } })
-  )
+  const { user, logout, accessToken } = useAuth(() => router.push({ pathname: '/login', query: { back: true } }))
 
   const getWard = async (id: string) => {
-    const wardService = new WardServiceClient(
-      'https://staging-api.helpwave.de/task-svc'
-    )
+    const wardService = new WardServiceClient('https://staging-api.helpwave.de/task-svc')
 
     const req = new GetWardRequest()
     req.setId(id)
 
-    wardService
-      .getWard(req, null)
+    wardService.getWard(req, null)
       .then((res) => {
         console.log('call successful', res.getId(), res.getName())
       })
@@ -33,15 +27,12 @@ const Home: NextPage = () => {
   }
 
   const createWard = async (name: string) => {
-    const wardService = new WardServiceClient(
-      'https://staging-api.helpwave.de/tasks-svc'
-    )
+    const wardService = new WardServiceClient('https://staging-api.helpwave.de/tasks-svc')
 
     const req = new CreateWardRequest()
     req.setName(name)
 
-    wardService
-      .createWard(req, null)
+    wardService.createWard(req, null)
       .then((res) => {
         console.log('call successful', res.getId(), res.getName())
       })
@@ -50,8 +41,7 @@ const Home: NextPage = () => {
       })
   }
 
-  const createRandomWard = async () =>
-    createWard(`random_ward_${Date.now().toString()}`)
+  const createRandomWard = async () => createWard(`random_ward_${Date.now().toString()}`)
 
   console.log(user, accessToken)
 
@@ -59,38 +49,20 @@ const Home: NextPage = () => {
 
   // TODO: proper navigation links in header (what should they be?)
   return (
-    <div className={tw('w-screen h-screen flex flex-col')}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
+      <PageWithHeader>
+        <Head>
+          <title>Create Next App</title>
+        </Head>
 
-      {/* all of these links are to be determined, these are just some placeholders */}
-      <Header
-        title="helpwave"
-        navigation={[
-          { text: 'Dashboard', href: '/' },
-          { text: 'Contact', href: '/contact' },
-        ]}
-        actions={[<UserMenu key="user-menu" user={user} />]}
-      />
+        <h1 className={tw('text-3xl font-bold underline')}>
+          {`Hello ${user.displayName}`}
+        </h1>
 
-      <h1 className={tw('text-3xl font-bold underline')}>
-        {user === null
-          ? 'Redirecting to log in page..'
-          : `Hello ${user.displayName}`}
-      </h1>
+        <button onClick={() => logout(() => window.location.reload())}>Logout</button>
 
-      <button onClick={() => logout(() => window.location.reload())}>
-        Logout
-      </button>
-
-      <button onClick={() => getWard('9af360fc-f612-4752-9982-a596c62e85c7')}>
-        Get ward (open console)
-      </button>
-      <button onClick={() => createRandomWard()}>
-        Create random ward (open console)
-      </button>
-    </div>
+        <button onClick={() => getWard('9af360fc-f612-4752-9982-a596c62e85c7')}>Get ward (open console)</button>
+        <button onClick={() => createRandomWard()}>Create random ward (open console)</button>
+      </PageWithHeader>
   )
 }
 

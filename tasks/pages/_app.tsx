eb29479cac262a86abe5f60c0f973fd05874b38e
DasProffi <felix.thape@gmail.com>
2023-04-13 10:52:31 +0200

    feat: add appstore to mobile interceptor

--- tasks/pages/_app.tsx
+++ tasks/pages/_app.tsx
@@ -7,7 +7,7 @@ import withNextApp from '@helpwave/common/twind/next/app'
 import { config } from '@helpwave/common/twind/config'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserView, MobileView } from 'react-device-detect'
-import { MobileInterceptor } from '../components/MobileInterceptor'
+import MobileInterceptor from '../components/MobileInterceptor'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -41,7 +41,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         </QueryClientProvider>
       </BrowserView>
       <MobileView>
-        <MobileInterceptor/>
+        <MobileInterceptor {...pageProps} />
       </MobileView>
     </ProvideLanguage>
   )

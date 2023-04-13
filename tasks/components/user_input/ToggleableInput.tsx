79c2a06e25b6468fbc52eafdc867c990eb5c4e07
DasProffi <felix.thape@gmail.com>
2023-04-13 23:31:31 +0200

    feat: add ToggleableInput and use it in patient form

---
+++ tasks/components/user_input/ToggleableInput.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import type { HTMLInputTypeAttribute, InputHTMLAttributes } from 'react'
+import { tw, tx } from '@helpwave/common/twind'
+import { Input, noop } from './Input'
+import Edit from '@helpwave/common/icons/Edit'
+
+type InputProps = {
+  /**
+   * used for the label's `for` attribute
+   */
+  id: string,
+  value: string,
+  label?: string,
+  /**
+   * @default 'text'
+   */
+  type?: HTMLInputTypeAttribute,
+  /**
+   * Callback for when the input's value changes
+   * This is pretty much required but made optional for the rare cases where it actually isn't need such as when used with disabled
+   * That could be enforced through a union type but that seems a bit overkill
+   * @default noop
+   */
+  onChange?: (text: string) => void,
+  labelClassName?: string
+} & Omit<InputHTMLAttributes<HTMLInputElement>, 'id' | 'value' | 'label' | 'type' | 'onChange' | 'crossOrigin'>
+
+export const ToggleableInput = ({
+  id,
+  type = 'text',
+  value,
+  label,
+  onChange = noop,
+  labelClassName,
+  ...restProps
+}: InputProps) => {
+  const [isEditing, setIsEditing] = useState(false)
+  return (
+    <div
+      className={tw('flex flex-row justify-between items-center')}
+      onClick={() => !isEditing ? setIsEditing(!isEditing) : undefined}
+    >
+      {isEditing ? (
+        <Input
+          autoFocus
+          {...restProps}
+          value={value}
+          type={type}
+          id={id}
+          label={label}
+          onChange={onChange}
+          onBlur={() => {
+            setIsEditing(false)
+          }}
+        />
+      ) :
+        <span className={tx(labelClassName ? labelClassName : 'text-xl font-semibold')}>{value}</span>
+      }
+      {!isEditing && <Edit className={tw('ml-2 scale-[80%] cursor-pointer')}/>}
+    </div>
+  )
+}

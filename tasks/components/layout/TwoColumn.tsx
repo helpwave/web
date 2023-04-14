82cb592053e31e81d80fd3edaef9540e88d90116
Florian Sabonchi <sabonchi@posteo.de>
2023-04-14 22:26:37 +0200

    feat: replace native scrollbar with simplebar-react fore TwoColumn.tsx

--- tasks/components/layout/TwoColumn.tsx
+++ tasks/components/layout/TwoColumn.tsx
@@ -1,6 +1,9 @@
 import { tw } from '@helpwave/common/twind'
 import type { ReactNode } from 'react'
 
+import SimpleBarReact from 'simplebar-react'
+import 'simplebar-react/dist/simplebar.min.css'
+
 type TwoColumnProps = {
   left: ReactNode,
   right: ReactNode
@@ -10,12 +13,16 @@ export const TwoColumn = ({ right, left }: TwoColumnProps) => {
   const headerHeight = 64
   return (
     <div className={tw(`flex flex-row h-[calc(100vh_-_${headerHeight}px)]`)}>
-      <div className={tw('overflow-auto w-7/12')}>
-        {left}
+      <div className={tw('overflow-hidden w-7/12')}>
+        <SimpleBarReact style={{ maxHeight: window.innerHeight - headerHeight }}>
+          {left}
+        </SimpleBarReact>
       </div>
       <div className={tw(`my-4 rounded-lg bg-gray-300 w-0.5`)}/>
-      <div className={tw('overflow-auto flex-1')}>
-        {right}
+      <div className={tw('overflow-hidden flex-1')}>
+        <SimpleBarReact style={{ maxHeight: window.innerHeight - headerHeight }}>
+          {right}
+        </SimpleBarReact>
       </div>
     </div>
   )

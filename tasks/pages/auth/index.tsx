3f62a34ba16be516bcd939c479c6e9d43b7af4fd
DasProffi <felix.thape@gmail.com>
2023-04-21 16:56:41 +0200

    fix: readd auth function from auth page

--- tasks/pages/auth/index.tsx
+++ tasks/pages/auth/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next'
+import { tw } from '@helpwave/common/twind'
 import { SignInButton } from '@helpwave/common/components/SignInButton'
+import { getAuthorizationUrl } from '../../utils/oauth'
 
 const Auth: NextPage = () => {
   return (
-    <>
-      <SignInButton/>
-    </>
+    <div className={tw('flex flex-col items-center w-screen h-screen justify-center')}>
+      <SignInButton
+        onClick={() => {
+          getAuthorizationUrl().then((url) => window.location.assign(url))
+        }}
+      />
+    </div>
   )
 }
 

fa517bd7aac335e443d44faedb339849d965bd79
DasProffi <felix.thape@gmail.com>
2023-04-23 01:57:44 +0200

    feat: change font to space grotesk

--- tasks/components/FeatureDisplay.tsx
+++ tasks/components/FeatureDisplay.tsx
@@ -18,12 +18,12 @@ export const FeatureDisplay = ({ feature }: FeatureDisplayProps) => {
     <Link href={feature.externResource} className={tw('flex flex-row gap-x-8 hover:bg-gray-100 rounded-xl p-3')}>
       <div className={tw('flex flex-col gap-x-2 w-1/3')}>
         <TimeDisplay date={feature.date} mode="date" />
-        <span className={tw('text-hw-primary-700 text-xl font-bold')}>{feature.title}</span>
+        <span className={tw('text-hw-primary-700 text-xl font-bold font-space')}>{feature.title}</span>
       </div>
       <div className={tw('flex flex-col gap-y-2 flex-1')}>
         {feature.description.map(value => value instanceof URL ?
           <img src={value.href} alt="" className={tw('h-auto w-full rounded-xl')}/> :
-          <span className={tw('font-semibold')}>{value}</span>)
+          <span className={tw('font-medium')}>{value}</span>)
         }
       </div>
     </Link>

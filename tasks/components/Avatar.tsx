35655992d60fe5aea7ae40462599aafccbe08745
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2023-04-25 15:32:46 +0200

    fix(web): alt text for profile image

--- tasks/components/Avatar.tsx
+++ tasks/components/Avatar.tsx
@@ -1,4 +1,4 @@
-import { tw, tx, css } from '@helpwave/common/twind/index'
+import { tw, tx } from '@helpwave/common/twind/index'
 
 export type AvatarProps = {
   avatarUrl: string,
@@ -6,25 +6,15 @@ export type AvatarProps = {
   size?: 'tiny' | 'small' | 'medium' | 'large'
 }
 
-const avatarStyle = css`
-.alt-text {
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-}
-`
-
 const Avatar = ({ avatarUrl, alt, size = 'medium' }: AvatarProps) => {
   return (
-    <div className={tw(avatarStyle)}>
-      <div className={tw('rounded-full')}>
-        <img className={tx('rounded-full border border-slate-200 group-hover:border-indigo-200 alt-text', {
-          'h-6 w-6': size === 'tiny',
-          'h-8 w-8': size === 'small',
-          'h-12 w-12': size === 'medium',
-          'h-16 w-16': size === 'large'
-        })} src={avatarUrl} alt={alt} />
-      </div>
+    <div className={tw('rounded-full')}>
+      <img className={tx('rounded-full border border-slate-200 group-hover:border-indigo-200  flex justify-evenly items-center', {
+        'h-6 w-6': size === 'tiny',
+        'h-8 w-8': size === 'small',
+        'h-12 w-12': size === 'medium',
+        'h-16 w-16': size === 'large'
+      })} src={avatarUrl} alt={alt} />
     </div>
   )
 }

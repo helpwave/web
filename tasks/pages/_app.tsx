15242f3d36dc143683c2efc906fc81e4c27075a3
Florian Jafar Sabonchi <sabonchi@posteo.de>
2023-05-01 19:46:37 +0200

    fix: mobile interceptor through media-query instead of MobileView and BrowserView

--- tasks/pages/_app.tsx
+++ tasks/pages/_app.tsx
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import { Inter, Space_Grotesk as SpaceGrotesk } from '@next/font/google'
-import { tw } from '@helpwave/common/twind'
+import { css, tw } from '@helpwave/common/twind'
 import { ProvideLanguage } from '@helpwave/common/hooks/useLanguage'
 import withNextApp from '@helpwave/common/twind/next/app'
 import { config } from '@helpwave/common/twind/config'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserView, MobileView } from 'react-device-detect'
 import MobileInterceptor from '../components/MobileInterceptor'
 import titleWrapper from '../utils/titleWrapper'
 
@@ -22,29 +21,45 @@ const spaceGrotesk = SpaceGrotesk({
 
 const queryClient = new QueryClient()
 
+const globalStyles = css`
+  @media (max-width: 1350px) {
+    .desktop-wrapper{
+      display: none;
+    }
+  }
+
+  @media (min-width: 1350px) {
+    .mobile-wrapper{
+      display: none;
+    }
+  }
+`
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ProvideLanguage>
-      <BrowserView>
-        <Head>
-          <title>{titleWrapper()}</title>
-          <style>{`
+    <div className={tw(globalStyles)}>
+      <ProvideLanguage>
+        <div className="desktop-wrapper">
+          <Head>
+            <title>{titleWrapper()}</title>
+            <style>{`
           :root {
             --font-inter: ${inter.style.fontFamily};
             --font-space: ${spaceGrotesk.style.fontFamily};
           }
         `}</style>
-        </Head>
-        <QueryClientProvider client={queryClient}>
-          <div className={tw('font-sans')} id="modal-root">
-            <Component {...pageProps} />
-          </div>
-        </QueryClientProvider>
-      </BrowserView>
-      <MobileView>
-        <MobileInterceptor {...pageProps} />
-      </MobileView>
-    </ProvideLanguage>
+          </Head>
+          <QueryClientProvider client={queryClient}>
+            <div className={tw('font-sans')} id="modal-root">
+              <Component {...pageProps} />
+            </div>
+          </QueryClientProvider>
+        </div>
+        <div className="mobile-wrapper">
+          <MobileInterceptor {...pageProps} />
+        </div>S
+      </ProvideLanguage>
+    </div>
   )
 }
 

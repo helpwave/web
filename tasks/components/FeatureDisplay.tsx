46765d661c0f94346f6ba6c5e27fbf648912aefe
DasProffi <felix.thape@gmail.com>
2023-05-02 11:34:52 +0200

    feat: make extern resource optional for features

--- tasks/components/FeatureDisplay.tsx
+++ tasks/components/FeatureDisplay.tsx
@@ -6,7 +6,7 @@ export type Feature = {
   title: string,
   date: Date,
   description: (string | URL)[],
-  externResource: URL
+  externResource?: URL
 }
 
 export type FeatureDisplayProps = {
@@ -17,18 +17,25 @@ export type FeatureDisplayProps = {
  * A component for showing a feature with a title, date of release and a link to a blog post on click
  */
 export const FeatureDisplay = ({ feature }: FeatureDisplayProps) => {
-  return (
-    <Link target="_blank" href={feature.externResource} className={tw('flex flex-row gap-x-8 hover:bg-gray-100 rounded-xl p-3')}>
+  const content = (
+    <>
       <div className={tw('flex flex-col gap-x-2 w-1/3')}>
-        <TimeDisplay date={feature.date} mode="date" />
+        <TimeDisplay date={feature.date} mode="date"/>
         <span className={tw('text-hw-primary-700 text-xl font-bold font-space')}>{feature.title}</span>
       </div>
       <div className={tw('flex flex-col gap-y-2 flex-1')}>
         {feature.description.map((value, index) => value instanceof URL ?
           <img key={index} src={value.href} alt="" className={tw('h-auto w-full rounded-xl')}/> :
-          <span key={index} className={tw('font-medium')}>{value}</span>)
-        }
+          <span key={index} className={tw('font-medium')}>{value}</span>)}
       </div>
-    </Link>
+    </>
   )
+  const tileStyle = 'flex flex-row gap-x-8 hover:bg-gray-100 rounded-xl p-3'
+
+  return feature.externResource !== undefined ? (
+        <Link target="_blank" href={feature.externResource}
+              className={tw(tileStyle)}>
+          {content}
+        </Link>
+  ) : <div className={tw(tileStyle)}>{content}</div>
 }

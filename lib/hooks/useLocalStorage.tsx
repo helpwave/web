b949839348e3fb5cfd9bdd5f7daef62ab90357f9
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2023-05-03 11:58:33 +0200

    feat: add Hook for localstorage

---
+++ lib/hooks/useLocalStorage.tsx
@@ -0,0 +1,41 @@
+import { createContext, useCallback, useContext, useEffect, useState } from 'react'
+import type { Dispatch, FunctionComponent, PropsWithChildren, SetStateAction } from 'react'
+import { LocalStorageService } from "../util/storage"
+
+declare global {
+  interface WindowEventMap {
+    'local-storage': CustomEvent
+  }
+}
+
+type SetValue<T> = Dispatch<SetStateAction<T>>
+type FunctionType = <T>(key: string, initValue: T) => [T, SetValue<T>]
+
+const useLocalStorage: FunctionType = <T, >(key: string, initValue: T) => {
+  const get = useCallback((): T => {
+    if (typeof window === "undefined") { 
+      return initValue
+    }
+    const storageService = new LocalStorageService()
+    const value = storageService.get<T>(key)
+    return value || initValue
+  }, [initValue, key])
+
+  const [storedValue, setStoredValue] = useState<T>(get)
+
+  const setValue: SetValue<T> = useCallback(value => {
+    const newValue = value instanceof Function ? value(storedValue) : value
+    const storageService = new LocalStorageService()
+    storageService.set(key, value)
+
+    setStoredValue(newValue)
+  }, [storedValue, setStoredValue, key])
+
+  useEffect(() => {
+    setStoredValue(get())
+  }, [])
+
+  return [storedValue, setValue]
+}
+
+export default useLocalStorage

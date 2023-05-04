52d07e488549242a663806a07eacbeb18aa9a445
Florian <Sabonchi@posteo.de>
2023-05-04 08:28:35 +0000

    fix: mobile interceptor through media-query instead of MobileView and… (#329)
    * fix: mobile interceptor through media-query instead of MobileView and BrowserView
    
    * fix: font family
    
    * feat: replace clasName through id
    
    * feat: update variable name
    
    * feat: add maxWidth variable for @media
    
    * feat: replace globalStyles with tw
    
    * feat: add custom screens for desktop and mobile
    
    * fix: remove react-device-detect package
    
    ---------
    
    Co-authored-by: Max Schäfer <max-schaefer-lg@gmx.de>

--- tasks/components/MobileInterceptor.tsx
+++ tasks/components/MobileInterceptor.tsx
@@ -4,26 +4,25 @@ import { useTranslation } from '@helpwave/common/hooks/useTranslation'
 import Link from 'next/link'
 import HelpwaveLogo from '@helpwave/common/icons/HelpwaveRect'
 import type { NextPage } from 'next'
-import { AndroidView, IOSView } from 'react-device-detect'
 import { getConfig } from '../utils/config'
 import { Span } from '@helpwave/common/components/Span'
 
 type MobileInterceptorTranslation = {
   pleaseDownloadApp: string,
-  playstore: string,
-  appstore: string
+  playStore: string,
+  appStore: string
 }
 
 const defaultMobileInterceptorTranslation = {
   en: {
     pleaseDownloadApp: 'Please download the app',
-    playstore: 'Google Play Store',
-    appstore: 'Apple App Store'
+    playStore: 'Google Play Store',
+    appStore: 'Apple App Store'
   },
   de: {
     pleaseDownloadApp: 'Bitte laden Sie die App herunter',
-    playstore: 'Google Play Store',
-    appstore: 'Apple App Store'
+    playStore: 'Google Play Store',
+    appStore: 'Apple App Store'
   }
 }
 
@@ -36,18 +35,14 @@ const defaultMobileInterceptorTranslation = {
 const MobileInterceptor: NextPage = ({ language }: PropsWithLanguage<MobileInterceptorTranslation>) => {
   const translation = useTranslation(language, defaultMobileInterceptorTranslation)
   const config = getConfig()
-  const playstoreLink = config.appstoreLinks.playStore
+  const playStoreLink = config.appstoreLinks.playStore
   const appstoreLink = config.appstoreLinks.appStore
   return (
     <div className={tw('w-screen h-[80vh] flex flex-col items-center justify-center')}>
       <HelpwaveLogo className={tw('w-1/3 mx-auto h-auto mb-2 text-black')}/>
       <Span type="subsectionTitle" className={tw('mb-8')}>{translation.pleaseDownloadApp}</Span>
-      <AndroidView>
-        <Link href={playstoreLink}>{translation.playstore}</Link>
-      </AndroidView>
-      <IOSView>
-        <Link href={appstoreLink}>{translation.appstore}</Link>
-      </IOSView>
+      <Link href={playStoreLink}>{translation.playStore}</Link>
+      <Link href={appstoreLink}>{translation.appStore}</Link>
     </div>
   )
 }
--- tasks/pages/_app.tsx
+++ tasks/pages/_app.tsx
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import { Inter, Space_Grotesk as SpaceGrotesk } from '@next/font/google'
-import { tw } from '@helpwave/common/twind'
+import { css, tw } from '@helpwave/common/twind'
 import { ProvideLanguage } from '@helpwave/common/hooks/useLanguage'
 import withNextApp from '@helpwave/common/twind/next/app'
 import { config } from '@helpwave/common/twind/config'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserView, MobileView } from 'react-device-detect'
 import MobileInterceptor from '../components/MobileInterceptor'
 import titleWrapper from '../utils/titleWrapper'
 
@@ -25,25 +24,26 @@ const queryClient = new QueryClient()
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ProvideLanguage>
-      <BrowserView>
+      <div className={tw('mobile:hidden')}>
         <Head>
           <title>{titleWrapper()}</title>
           <style>{`
-          :root {
-            --font-inter: ${inter.style.fontFamily};
-            --font-space: ${spaceGrotesk.style.fontFamily};
-          }
-        `}</style>
+        :root {
+          --font-inter: ${inter.style.fontFamily};
+          --font-space: ${spaceGrotesk.style.fontFamily};
+        }
+      `}</style>
         </Head>
         <QueryClientProvider client={queryClient}>
           <div className={tw('font-sans')} id="modal-root">
             <Component {...pageProps} />
           </div>
         </QueryClientProvider>
-      </BrowserView>
-      <MobileView>
+      </div>
+
+      <div className={tw('desktop:hidden')}>
         <MobileInterceptor {...pageProps} />
-      </MobileView>
+      </div>
     </ProvideLanguage>
   )
 }

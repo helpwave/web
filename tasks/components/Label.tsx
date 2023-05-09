482cd25087d9458f8d6c39138690c51f934ea357
Siva Sankar Reddy Bogala <shivadharmi@gmail.com>
2023-05-09 19:19:05 +0530

    fix: combine className in one condition

--- tasks/components/Label.tsx
+++ tasks/components/Label.tsx
@@ -2,7 +2,7 @@ import { Span } from '@helpwave/common/components/Span'
 import { tx } from '@helpwave/common/twind'
 import type { PropsWithChildren } from 'react'
 
-type ColorTypes = 'label-1' | 'label-2' | 'label-3' | 'blue' | 'pink' // extended these colors for more variations
+type ColorTypes = 'label-1' | 'label-2' | 'label-3' | 'blue' | 'pink'; // extended these colors for more variations
 
 export type LabelProps = PropsWithChildren<{
   name: string,
@@ -21,16 +21,11 @@ const Label = ({
   return (
     <Span
       className={tx(`rounded-full text-xs font-bold py-1 px-2`, className, {
-        'bg-hw-label-pink-background': color === 'pink',
-        'text-hw-label-pink-text': color === 'pink',
-        'bg-hw-label-blue-background': color === 'blue',
-        'text-hw-label-blue-text': color === 'blue',
-        'bg-hw-label-1-background': color === 'label-1',
-        'text-hw-label-1-text': color === 'label-1',
-        'bg-hw-label-2-background': color === 'label-2',
-        'text-hw-label-2-text': color === 'label-2',
-        'bg-hw-label-3-background': color === 'label-3',
-        'text-hw-label-3-text': color === 'label-3',
+        'bg-hw-label-pink-background text-hw-label-pink-text': color === 'pink',
+        'bg-hw-label-blue-background text-hw-label-blue-text': color === 'blue',
+        'bg-hw-label-1-background text-hw-label-1-text': color === 'label-1',
+        'bg-hw-label-2-background text-hw-label-2-text': color === 'label-2',
+        'bg-hw-label-3-background text-hw-label-3-text': color === 'label-3',
       })}
     >
       {name}

6a7fe798154b3bfecb2c0c4cf1242e0f97714bd1
Florian Jafar Sabonchi <sabonchi@posteo.de>
2023-05-13 09:40:45 +0200

    chore: add disclaimer text for Textarea component

--- lib/components/user_input/Textarea.tsx
+++ lib/components/user_input/Textarea.tsx
@@ -1,11 +1,13 @@
 import { tw, tx, css } from '../../twind'
 import type { TextareaHTMLAttributes } from 'react'
+import { useState } from 'react'
 
 type TextareaProps = {
   headline?: string,
   id?: string,
   resizable?: boolean,
-  onChange?: (text: string) => void
+  onChange?: (text: string) => void,
+  disclaimer?: string
 } & Omit<TextareaHTMLAttributes<Element>, 'id' | 'onChange'>
 
 const noop = () => { /* noop */ }
@@ -22,7 +24,9 @@ const globalStyles = css`
  *
  * The State is managed by the parent
  */
-export const Textarea = ({ headline, id, resizable = false, onChange = noop, ...props }: TextareaProps) => {
+export const Textarea = ({ headline, id, resizable = false, onChange = noop, disclaimer, ...props }: TextareaProps) => {
+  const [hasFocus, setHasFocus] = useState(false)
+
   return (
     <div className={tw(globalStyles)}>
       <div className={`textarea-wrapper ${tw('relative shadow border-2 border-gray-300 rounded-lg')}`}>
@@ -33,10 +37,21 @@ export const Textarea = ({ headline, id, resizable = false, onChange = noop, ...
           id={id}
           className={tx('pt-0 border-transparent focus:border-transparent focus:ring-0 h-32 appearance-none border w-full text-gray-700 leading-tight focus:outline-none', { 'resize-none': !resizable })}
           onChange={(event) => onChange(event.target.value)}
+          onFocus={() => {
+            setHasFocus(true)
+          }}
+          onBlur={() => {
+            setHasFocus(false)
+          }}
           {...props}
         >
       </textarea>
       </div>
+      {hasFocus && (
+        <label className={tw('text-hw-negative-500')}>
+          {disclaimer}
+        </label>
+      )}
     </div>
   )
 }

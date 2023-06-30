889e6796da1915a687a61f1f64a8b35159d7cb50
Felix <13922835+use-to@users.noreply.github.com>
2023-06-30 21:28:08 +0200

    chore: update spotify link

--- landing-page/components/Footer.tsx
+++ landing-page/components/Footer.tsx
@@ -8,7 +8,7 @@ const socials = [
   { name: 'Twitter', link: 'https://twitter.com/helpwave_org' },
   { name: 'YouTube', link: 'https://www.youtube.com/@helpwave' },
   { name: 'Twitch', link: 'https://www.twitch.tv/helpwave' },
-  { name: 'Spotify', link: 'https://open.spotify.com/show/7oRRTH65cGS5yvvcX8XXOb' },
+  { name: 'Spotify', link: 'https://open.spotify.com/show/6hL5UMytp81gmURnfn3dS9' },
 ]
 
 const Footer = () => {

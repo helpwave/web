870f6ed23730d1f062aaa8ffeed0665eafc41dc7
Felix <67233923+DasProffi@users.noreply.github.com>
2023-07-25 18:36:41 +0200

    refactor: improve readability
    Co-authored-by: Felix <13922835+use-to@users.noreply.github.com>

--- tasks/components/SubtaskView.tsx
+++ tasks/components/SubtaskView.tsx
@@ -101,9 +101,11 @@ export const SubtaskView = ({
                 }}
                 onDoneChange={done => {
                   // taskTemplateId === "" for the creation of a template
-                  if (taskTemplateId === undefined) {
-                    subtask.isDone = done
+                   if (taskTemplateId) {
+                    return
                   }
+                  
+                  subtask.isDone = done
                 }}
               />
             ))}

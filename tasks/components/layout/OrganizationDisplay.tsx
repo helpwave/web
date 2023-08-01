ebdb94c265db437b32540f16575778b026a2752d
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2023-08-01 21:58:42 +0200

    fix: scaling issues

--- tasks/components/layout/OrganizationDisplay.tsx
+++ tasks/components/layout/OrganizationDisplay.tsx
@@ -49,7 +49,7 @@ export const OrganizationDisplay = ({
   const { data } = useOrganizationsByUserQuery()
   const usedOrganizations: OrganizationDTO[] = organizations ?? data ?? []
 
-  const columns = width === undefined ? 2 : Math.max(Math.floor(width / 250), 1)
+  const columns = width === undefined ? 3 : Math.min(Math.floor(width / 250), 3)
 
   const usedSelectedID = selectedOrganizationID ?? context.state.organizationID
   return (

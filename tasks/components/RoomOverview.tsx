ae9cfd0bc23b88ae1d4c1983c277e24a9400aae3
DasProffi <felix.thape@gmail.com>
2023-08-02 14:13:20 +0200

    fix: fix column calculation to be done instantly and update max columns

--- tasks/components/RoomOverview.tsx
+++ tasks/components/RoomOverview.tsx
@@ -4,7 +4,7 @@ import { BedCard } from './cards/BedCard'
 import type { RoomOverviewDTO } from '../mutations/room_mutations'
 import { Span } from '@helpwave/common/components/Span'
 import type { BedMinimalDTO } from '../mutations/bed_mutations'
-import { useContext, useRef } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { WardOverviewContext } from '../pages/ward/[uuid]'
 import type { PatientDTO } from '../mutations/patient_mutations'
 import { emptyPatient } from '../mutations/patient_mutations'
@@ -19,6 +19,7 @@ export type RoomOverviewProps = {
 export const RoomOverview = ({ room }: RoomOverviewProps) => {
   const context = useContext(WardOverviewContext)
   const ref = useRef<HTMLDivElement>(null)
+  const [columns, setColumns] = useState(3)
 
   const setSelectedBed = (room: RoomOverviewDTO, bed: BedMinimalDTO, patient: PatientDTO|undefined) =>
     context.updateContext({
@@ -28,18 +29,20 @@ export const RoomOverview = ({ room }: RoomOverviewProps) => {
       patient
     })
 
+  useEffect(() => {
+    if (ref.current?.offsetWidth) {
+      setColumns(Math.min(Math.max(1, Math.floor(ref.current?.offsetWidth / 200)), 4))
+    }
+  }, [ref.current?.offsetWidth])
+
   const selectedBedID = context.state.bed?.id
-  const width = ref.current?.offsetWidth
-  const columns = width ? Math.max(1, Math.floor(width / 200)) : 3
 
   return (
-    <div ref={ref}>
-      { room.beds.length > 0 ? (
-        <div className={tw('flex flex-row items-center mb-1')}>
-          <div className={tw('w-2 h-2 mx-2 rounded-full bg-gray-300')}/>
-          <Span type="subsectionTitle">{room.name}</Span>
-        </div>
-      ) : ''}
+    <div className={tw('flex flex-col w-full')} ref={ref}>
+      <div className={tw('flex flex-row items-center mb-1')}>
+        <div className={tw('w-2 h-2 mx-2 rounded-full bg-gray-300')}/>
+        <Span type="subsectionTitle">{room.name}</Span>
+      </div>
       <div className={tw(`grid grid-cols-${columns} gap-4`)}>
         {room.beds.map(bed => bed.patient && bed.patient?.id ?
             (
@@ -54,7 +57,10 @@ export const RoomOverview = ({ room }: RoomOverviewProps) => {
                 event.stopPropagation()
                 if (bed.patient) {
                   // LINTER: `bed.patient.id` gets evaluated as undefined without this if
-                  setSelectedBed(room, bed, { ...emptyPatient, id: bed.patient.id })
+                  setSelectedBed(room, bed, {
+                    ...emptyPatient,
+                    id: bed.patient.id
+                  })
                 }
               }}
               isSelected={selectedBedID === bed.id}

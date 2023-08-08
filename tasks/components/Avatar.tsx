9f9e7cc02a9dcc40779b6ad69328f3471c993fd5
Felix <67233923+DasProffi@users.noreply.github.com>
2023-08-08 10:29:14 +0200

    fix: fix OrganizationMemberList overflow

--- tasks/components/Avatar.tsx
+++ tasks/components/Avatar.tsx
@@ -14,10 +14,10 @@ const Avatar = ({ avatarUrl, alt, size = 'medium' }: AvatarProps) => {
   return (
     <div className={tw('rounded-full')}>
       <img className={tx('rounded-full border border-slate-200 group-hover:border-indigo-200  flex justify-evenly items-center', {
-        'h-6 w-6': size === 'tiny',
-        'h-8 w-8': size === 'small',
-        'h-12 w-12': size === 'medium',
-        'h-16 w-16': size === 'large'
+        'h-6 w-6 min-h-[24px] min-w-[24px]': size === 'tiny',
+        'h-8 w-8 min-h-[32px] min-w-[32px]': size === 'small',
+        'h-12 w-12 min-h-[48px] min-w-[48px]': size === 'medium',
+        'h-16 w-16 min-h-[64px] min-w-[64px]': size === 'large'
       })} src={avatarUrl} alt={alt} />
     </div>
   )
--- tasks/components/OrganizationMemberList.tsx
+++ tasks/components/OrganizationMemberList.tsx
@@ -21,6 +21,7 @@ import {
   useRemoveMemberMutation
 } from '../mutations/organization_mutations'
 import { OrganizationContext } from '../pages/organizations'
+import { LoadingAndErrorComponent } from '@helpwave/common/components/LoadingAndErrorComponent'
 
 type OrganizationMemberListTranslation = {
   edit: string,
@@ -97,16 +98,6 @@ export const OrganizationMemberList = ({
 
   const [deleteDialogState, setDeleteDialogState] = useState<DeleteDialogState>(defaultDeleteDialogState)
 
-  // TODO add view for loading
-  if (isLoading && !members) {
-    return <div>Loading Widget</div>
-  }
-
-  // TODO add view for error or error handling
-  if (isError && !members) {
-    return <div>Error Message</div>
-  }
-
   const hasSelectedMultiple = !!tableState.selection && tableState.selection.currentSelection.length > 1
   const idMapping = (dataObject: OrgMember) => dataObject.id
 
@@ -131,61 +122,68 @@ export const OrganizationMemberList = ({
         }}
         confirmType="negative"
       />
-      <div className={tw('flex flex-row justify-between items-center mb-2')}>
-        <Span type="tableName">{translation.members + ` (${usedMembers.length})`}</Span>
-        <div className={tw('flex flex-row gap-x-2')}>
-          {tableState.selection && tableState.selection.currentSelection.length > 0 && (
-            <Button
-              onClick={() => setDeleteDialogState({ isShowing: true })}
-              color="negative"
-            >
-              {translation.removeSelection}
-            </Button>
-          )}
-        </div>
-      </div>
-      <Table
-        data={usedMembers}
-        stateManagement={[tableState, setTableState]}
-        header={[
-          <div key="member" className={tw('flex flex-row')}>
-            <Span type="tableHeader">{translation.member}</Span>
-          </div>,
-          <div key="role" className={tw('flex flex-row')}>
-            <Span type="tableHeader">{translation.role}</Span>
-          </div>,
-          <></>
-        ]}
-        rowMappingToCells={dataObject => [
-          <div key="member" className={tw('flex flex-row items-center h-12')}>
-            <Avatar avatarUrl={dataObject.avatarURL} alt="" size="small"/>
-            <div className={tw('flex flex-col ml-2 max-w-[250px] overflow-hidden')}>
-              <Span className={tw('font-bold truncate')}>{dataObject.name}</Span>
-              <a href={`mailto:${dataObject.email}`} >
-                <Span type="description" className={tw('text-sm truncate')}>{dataObject.email}</Span>
-              </a>
-            </div>
-          </div>,
-          <div key="role" className={tw('flex flex-row items-center mr-2')}>
-            <button className={tw('flex flex-row items-center')} onClick={() => { /* TODO allow changing roles */
-            }}>
-              <Span className={tw(`font-semibold`)}>
-                {translation.roleTypes[dataObject.role]}
-              </Span>
-            </button>
-          </div>,
-          <div key="remove" className={tw('flex flex-row justify-end')}>
-            <Button
-              onClick={() => setDeleteDialogState({ isShowing: true, member: dataObject })}
-              color="negative"
-              variant="textButton"
-            >
-              {translation.remove}
-            </Button>
+      <LoadingAndErrorComponent
+        hasError={(isError || !data) && !members}
+        isLoading={!members && isLoading}
+        errorProps={{ classname: tw('border-2 border-gray-600 rounded-xl min-h-[300px]') }}
+        loadingProps={{ classname: tw('border-2 border-gray-600 rounded-xl min-h-[300px]') }}
+      >
+        <div className={tw('flex flex-row justify-between items-center mb-2')}>
+          <Span type="tableName">{translation.members + ` (${usedMembers.length})`}</Span>
+          <div className={tw('flex flex-row gap-x-2')}>
+            {tableState.selection && tableState.selection.currentSelection.length > 0 && (
+              <Button
+                onClick={() => setDeleteDialogState({ isShowing: true })}
+                color="negative"
+              >
+                {translation.removeSelection}
+              </Button>
+            )}
           </div>
-        ]}
-       identifierMapping={idMapping}
-      />
+        </div>
+        <Table
+          data={usedMembers}
+          stateManagement={[tableState, setTableState]}
+          header={[
+            <div key="member" className={tw('flex flex-row')}>
+              <Span type="tableHeader">{translation.member}</Span>
+            </div>,
+            <div key="role" className={tw('flex flex-row')}>
+              <Span type="tableHeader">{translation.role}</Span>
+            </div>,
+            <></>
+          ]}
+          rowMappingToCells={dataObject => [
+            <div key="member" className={tw('flex flex-row items-center h-12 overflow-hidden max-w-[200px]')}>
+              <Avatar avatarUrl={dataObject.avatarURL} alt="" size="small"/>
+              <div className={tw('flex flex-col ml-2')}>
+                <Span className={tw('font-bold truncate')}>{dataObject.name}</Span>
+                <a href={`mailto:${dataObject.email}`}>
+                  <Span type="description" className={tw('text-sm truncate')}>{dataObject.email}</Span>
+                </a>
+              </div>
+            </div>,
+            <div key="role" className={tw('flex flex-row items-center mr-2')}>
+              <button className={tw('flex flex-row items-center')} onClick={() => { /* TODO allow changing roles */
+              }}>
+                <Span className={tw(`font-semibold`)}>
+                  {translation.roleTypes[dataObject.role]}
+                </Span>
+              </button>
+            </div>,
+            <div key="remove" className={tw('flex flex-row justify-end')}>
+              <Button
+                onClick={() => setDeleteDialogState({ isShowing: true, member: dataObject })}
+                color="negative"
+                variant="textButton"
+              >
+                {translation.remove}
+              </Button>
+            </div>
+          ]}
+         identifierMapping={idMapping}
+        />
+      </LoadingAndErrorComponent>
     </div>
   )
 }

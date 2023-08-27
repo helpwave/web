f059435ddfcd2030d2e821d518a004ee2627d58e
DasProffi <felix.thape@gmail.com>
2023-08-27 17:41:34 +0200

    chore: use feedUrl from config again

--- tasks/utils/news.ts
+++ tasks/utils/news.ts
@@ -1,10 +1,9 @@
-// import { getConfig } from './config'
+import { getConfig } from './config'
 import type { LocalizedNews } from '@helpwave/common/util/news'
 import { localizedNewsSchema } from '@helpwave/common/util/news'
 
 export const fetchLocalizedNews = (): Promise<LocalizedNews> => {
-  // TODO change later back
-  const { featuresFeedUrl } = { featuresFeedUrl: 'https://cdn.helpwave.io/feed_intl.json' }// getConfig()
+  const { featuresFeedUrl } = getConfig()
   return fetch(featuresFeedUrl)
     .then((res) => res.json())
     .then(async (json) => {

10bab3605a4457b0875443b4c3639e92c7e620ce
Felix Thape <felix.thape@gmail.com>
2023-08-31 02:44:16 +0200

    feat: update get members request

--- tasks/mutations/organization_member_mutations.ts
+++ tasks/mutations/organization_member_mutations.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
-import { organizationQueryKey } from './organization_mutations'
+import { GetMembersByOrganizationRequest } from '@helpwave/proto-ts/proto/services/user_svc/v1/organization_svc_pb'
+import { getAuthenticatedGrpcMetadata, organizationService } from '../utils/grpc'
 
 // TODO replace later
 export const enum Role {
@@ -19,16 +20,32 @@ export type OrgMemberMinimalDTO = {
   id: string
 }
 
-// TODO MembersByOrganization
+const membersQueryKey = 'membersQueryKey'
 export const useMembersByOrganizationQuery = (organizationId: string | undefined) => {
   return useQuery({
-    queryKey: [organizationQueryKey],
+    queryKey: [membersQueryKey],
     enabled: !!organizationId,
     queryFn: async () => {
       if (!organizationId) {
         return []
       }
-      const members: OrgMember[] = []
+
+      const req = new GetMembersByOrganizationRequest()
+      req.setId(organizationId)
+
+      const res = await organizationService.getMembersByOrganization(req, getAuthenticatedGrpcMetadata())
+
+      if (!res.toObject()) {
+        console.error('error in MembersByOrganization')
+      }
+
+      const members: OrgMember[] = res.getMembersList().map(member => ({
+        id: member.getUserId(),
+        name: member.getNickname(),
+        email: member.getEmail(),
+        avatarURL: member.getAvatarUrl(),
+        role: Role.admin, // TODO update this
+      }))
       return members
     },
   })

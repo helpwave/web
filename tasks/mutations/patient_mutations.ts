1c12b40618835c3551e84fced8eefac18500c847
Felix Thape <felix.thape@gmail.com>
2023-09-24 14:14:19 +0200

    feat: add patient readmit mutation

--- tasks/mutations/patient_mutations.ts
+++ tasks/mutations/patient_mutations.ts
@@ -12,7 +12,7 @@ import {
   GetPatientDetailsResponse,
   GetPatientAssignmentByWardRequest,
   GetPatientListRequest,
-  DeletePatientRequest
+  DeletePatientRequest, ReadmitPatientRequest
 } from '@helpwave/proto-ts/proto/services/task_svc/v1/patient_svc_pb'
 import { patientService, getAuthenticatedGrpcMetadata } from '../utils/grpc'
 import type { BedWithPatientId } from './bed_mutations'
@@ -375,6 +375,24 @@ export const useDeletePatientMutation = () => {
       req.setId(patientId)
 
       const res = await patientService.deletePatient(req, getAuthenticatedGrpcMetadata())
+
+      return res.toObject()
+    },
+    onSuccess: () => {
+      queryClient.refetchQueries([roomsQueryKey, roomOverviewsQueryKey]).then()
+      queryClient.refetchQueries([patientsQueryKey]).then()
+    }
+  })
+}
+
+export const useReadmitPatientMutation = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async (patientId: string) => {
+      const req = new ReadmitPatientRequest()
+      req.setPatientId(patientId)
+
+      const res = await patientService.readmitPatient(req, getAuthenticatedGrpcMetadata())
 
       return res.toObject()
     },

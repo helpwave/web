119b7d57b7fa667f4865890a00c67826a9fcaa42
DasProffi <felix.thape@gmail.com>
2023-10-02 16:39:14 +0200

    feat: add recent patients

--- tasks/components/layout/DashboardDisplay.tsx
+++ tasks/components/layout/DashboardDisplay.tsx
@@ -1,4 +1,4 @@
-import { tw } from '@helpwave/common/twind'
+import { tw, tx } from '@helpwave/common/twind'
 import type { Languages } from '@helpwave/common/hooks/useLanguage'
 import type { PropsWithLanguage } from '@helpwave/common/hooks/useTranslation'
 import { useTranslation } from '@helpwave/common/hooks/useTranslation'
@@ -13,8 +13,11 @@ import { InvitationBanner } from '../InvitationBanner'
 import { LoadingAndErrorComponent } from '@helpwave/common/components/LoadingAndErrorComponent'
 import { useAuth } from '../../hooks/useAuth'
 import { getConfig } from '../../utils/config'
+import { useRecentPatientsQuery } from '../../mutations/patient_mutations'
+import { PatientCard } from '../cards/PatientCard'
 
 type DashboardDisplayTranslation = {
+  patients: string,
   organizations: string,
   wards: string,
   recent: string,
@@ -23,12 +26,14 @@ type DashboardDisplayTranslation = {
 
 const defaultDashboardDisplayTranslations: Record<Languages, DashboardDisplayTranslation> = {
   en: {
+    patients: 'Patients',
     organizations: 'Organizations',
     wards: 'Wards',
     recent: 'Recently used',
     showAllOrganizations: 'Show all organizations'
   },
   de: {
+    patients: 'Patienten',
     organizations: 'Organisationen',
     wards: 'Stationen',
     recent: 'Kürzlich Benutzt',
@@ -54,50 +59,76 @@ export const DashboardDisplay = ({
   const minimumWidthOfCards = 220 // the value of much space a card and the surrounding gap requires, given in px
   const columns = !width ? 3 : Math.max(Math.floor(width / minimumWidthOfCards), 1)
   const { organizations: tokenOrganizations } = useAuth()
-  const { data: wards, isLoading: isLoadingWards } = useWardOverviewsQuery(organizations.length > 0 ? organizations[0].id : undefined)
+  const {
+    data: wards,
+    isLoading: isLoadingWards
+  } = useWardOverviewsQuery(organizations.length > 0 ? organizations[0].id : undefined)
   const { fakeTokenEnable } = getConfig()
-
+  const {
+    data: patients,
+    isLoading: isLoadingPatients
+  } = useRecentPatientsQuery()
   organizations = organizations.filter((organization) => fakeTokenEnable || tokenOrganizations.includes(organization.id))
 
   return (
     <div className={tw('flex flex-col py-4 px-6 gap-y-4')}>
-      <InvitationBanner />
-      <div className={tw('flex flex-col')}>
-        <div className={tw('flex flex-row justify-between mb-1')}>
-          <div className={tw('flex flex-col')}>
-            <Span className={tw('font-bold text-gray-700')}>{translation.recent}</Span>
-            <Span type="subsectionTitle">{translation.organizations}</Span>
-          </div>
-          <div>
-            <Button
-              color="accent"
-              onClick={() => router.push('/organizations')}
-            >
-              {translation.showAllOrganizations}
-            </Button>
-          </div>
-        </div>
-        <div className={tw(`grid grid-cols-${columns} gap-6`)}>
-          {organizations.map(organization => (
-            <OrganizationCard
-              key={organization.id}
-              organization={organization}
-              onTileClick={() => router.push(`/organizations/${organization.id}`)}
-            />
-          ))}
-        </div>
-      </div>
+      <InvitationBanner/>
+      <Span type="title">{translation.recent}</Span>
+      <LoadingAndErrorComponent
+        isLoading={isLoadingPatients}
+      >
+        <Span type="subsectionTitle">{translation.patients}</Span>
+        {patients?.map(patient => (
+          <PatientCard
+            key={patient.id}
+            className={tx({ '!cursor-not-allowed': !patient.wardId })}
+            bedName={patient.bed?.name}
+            patientName={patient.name}
+            onTileClick={() => patient.wardId ? router.push(`/ward/${patient.wardId}`) : undefined}
+          />
+        ))}
+      </LoadingAndErrorComponent>
       {organizations.length > 0 && (
         <LoadingAndErrorComponent isLoading={isLoadingWards}>
-            <div className={tw('flex flex-col gap-y-1')}>
-              <Span type="subsectionTitle">{translation.wards}</Span>
-              {wards && wards.length > 0 ? (
-                <div className={tw(`grid grid-cols-${columns} gap-6`)}>
-                  {wards?.map(ward => <WardCard key={ward.id} ward={ward} onTileClick={() => router.push(`/ward/${ward.id}`)}/>)}
-                </div>
-              ) : <Span type="accent">No wards</Span>}
-            </div>
+          <div className={tw('flex flex-col gap-y-1')}>
+            <Span type="subsectionTitle">{translation.wards}</Span>
+            {wards && wards.length > 0 ? (
+              <div className={tw(`grid grid-cols-${columns} gap-6`)}>
+                {wards?.map(ward => (
+                  <WardCard
+                    key={ward.id}
+                    ward={ward}
+                    onTileClick={() => router.push(`/ward/${ward.id}`)}
+                  />
+                ))}
+              </div>
+            ) : <Span type="accent">No wards</Span>}
+          </div>
         </LoadingAndErrorComponent>
+      )}
+      {organizations && (
+        <div>
+          <div className={tw('flex flex-row gap-x-2 justify-between mb-4')}>
+            <Span type="subsectionTitle">{translation.organizations}</Span>
+            <div>
+              <Button
+                color="accent"
+                onClick={() => router.push('/organizations')}
+              >
+                {translation.showAllOrganizations}
+              </Button>
+            </div>
+          </div>
+          <div className={tw(`grid grid-cols-${columns} gap-6`)}>
+            {organizations.map(organization => (
+              <OrganizationCard
+                key={organization.id}
+                organization={organization}
+                onTileClick={() => router.push(`/organizations/${organization.id}`)}
+              />
+            ))}
+          </div>
+        </div>
       )}
     </div>
   )
--- tasks/mutations/patient_mutations.ts
+++ tasks/mutations/patient_mutations.ts
@@ -13,12 +13,12 @@ import {
   GetPatientAssignmentByWardRequest,
   GetPatientListRequest,
   DeletePatientRequest,
-  ReadmitPatientRequest
+  ReadmitPatientRequest, GetRecentPatientsRequest
 } from '@helpwave/proto-ts/proto/services/task_svc/v1/patient_svc_pb'
 import { patientService, getAuthenticatedGrpcMetadata } from '../utils/grpc'
 import type { BedWithPatientId } from './bed_mutations'
 import type { RoomWithMinimalBedAndPatient } from './room_mutations'
-import { roomOverviewsQueryKey, roomsQueryKey } from './room_mutations'
+import { roomOverviewsQueryKey, roomsQueryKey, useRoomQuery } from './room_mutations'
 import { noop } from '@helpwave/common/util/noop'
 
 export type PatientMinimalDTO = {
@@ -59,18 +59,24 @@ export type PatientWithBedAndRoomDTO = PatientMinimalDTO & {
   bed: { id: string, name: string }
 }
 
+export type RecentPatientDTO = PatientMinimalDTO & {
+  wardId?: string,
+  room?: { id: string, name: string },
+  bed?: { id: string, name: string }
+}
+
 export type PatientListDTO = {
   active: PatientWithBedAndRoomDTO[],
   unassigned: PatientMinimalDTO[],
   discharged: PatientMinimalDTO[]
 }
 
-export type PatientWithBedIdDTO = PatientMinimalDTO &{
+export type PatientWithBedIdDTO = PatientMinimalDTO & {
   note: string,
   bedId: string
 }
 
-export type PatientDetailsDTO = PatientMinimalDTO &{
+export type PatientDetailsDTO = PatientMinimalDTO & {
   note: string,
   tasks: TaskDTO[]
 }
@@ -221,8 +227,14 @@ export const usePatientListQuery = (organisationId?: string, wardId?: string) =>
           return ({
             id: value.getId(),
             name: value.getHumanReadableIdentifier(),
-            bed: { id: bed?.getId() ?? '', name: bed?.getName() ?? '' },
-            room: { id: room?.getId() ?? '', name: room?.getName() ?? '' }
+            bed: {
+              id: bed?.getId() ?? '',
+              name: bed?.getName() ?? ''
+            },
+            room: {
+              id: room?.getId() ?? '',
+              name: room?.getName() ?? ''
+            }
           })
         }),
         discharged: res.getDischargedPatientsList().map(value => ({
@@ -236,6 +248,42 @@ export const usePatientListQuery = (organisationId?: string, wardId?: string) =>
       }
 
       return patientList
+    }
+  })
+}
+
+export const useRecentPatientsQuery = () => {
+  const query = useRoomQuery
+  return useQuery({
+    queryKey: [patientsQueryKey],
+    queryFn: async () => {
+      const req = new GetRecentPatientsRequest()
+      const res = await patientService.getRecentPatients(req, getAuthenticatedGrpcMetadata())
+
+      const patients: RecentPatientDTO[] = []
+      for (const patient of res.getRecentPatientsList()) {
+        const room = patient.getRoom()
+        const bed = patient.getBed()
+        let wardId: string | undefined // TODO get wardId from query once implemented
+        if (room) {
+          const roomByQuery = await query(room.getId())
+          if (roomByQuery.data) {
+            wardId = roomByQuery.data.id
+          } else {
+            console.error('RecentPatientsQuery failed room not loaded')
+          }
+        }
+
+        patients.push({
+          id: patient.getId(),
+          name: patient.getHumanReadableIdentifier(),
+          wardId,
+          bed: bed ? { id: bed.getId(), name: bed.getName() } : undefined,
+          room: room ? { id: room.getId(), name: room.getId() } : undefined
+        })
+      }
+
+      return patients
     }
   })
 }
@@ -257,7 +305,10 @@ export const usePatientCreateMutation = (organisationId: string, callback: (pati
         console.error('create room failed')
       }
 
-      patient = { ...patient, id: res.getId() }
+      patient = {
+        ...patient,
+        id: res.getId()
+      }
 
       callback(patient)
       return patient

025f6e4441b6407a8195838fbf751b8d7184134e
use-to <13922835+use-to@users.noreply.github.com>
2023-10-10 11:47:48 +0200

    feat: update og definitions including twitter

--- landing-page/pages/_document.tsx
+++ landing-page/pages/_document.tsx
@@ -13,14 +13,22 @@ class MyDocument extends Document {
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="white" />
           <meta name="description" content="Next generation healthcare software approach!" />
           <meta property="og:title" name="og:title" content="helpwave" />
+          <meta property="og:site_name" content="helpwave" />
           <meta property="og:image" content="https://cdn.helpwave.de/thumbnail.png" />
+          <meta property="og:image:url" content="https://cdn.helpwave.de/thumbnail.png" />
+          <meta property="og:image:secure_url" content="https://cdn.helpwave.de/thumbnail.png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="627" />
+          <meta property="og:image:alt" content="helpwave: empowering medical heroes, united in technology" />
           <meta property="og:description" content="Next generation healthcare software approach!" />
           <meta property="og:url" content="https://helpwave.de/" />
           <meta property="og:type" content="website" />
           <meta name="twitter:card" content="summary" />
-
+          <meta name="twitter:site" content="@helpwave_org" />
+          <meta name="twitter:creator" content="@helpwave_org" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:locale:alternate" content="en_GB" />
+          <meta property="og:locale:alternate" content="de_DE" />
         </Head>
         <body>
           <Main />

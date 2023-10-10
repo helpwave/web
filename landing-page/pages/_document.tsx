27a9923e412b3283d3fa237f54c59402cbd6df31
use-to <13922835+use-to@users.noreply.github.com>
2023-10-10 11:22:48 +0200

    feat: add thumbnail for open graph

--- landing-page/pages/_document.tsx
+++ landing-page/pages/_document.tsx
@@ -10,13 +10,17 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="canonical" href="https://helpwave.de/" />
-          <meta name="theme-color" media="(prefers-color-scheme: dark)" content="black" />
+          <meta name="theme-color" media="(prefers-color-scheme: light)" content="white" />
           <meta name="description" content="Next generation healthcare software approach!" />
-          <meta name="og:title" property="og:title" content="helpwave" />
+          <meta property="og:title" name="og:title" content="helpwave" />
+          <meta property="og:image" content="https://cdn.helpwave.de/thumbnail.png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="627" />
           <meta property="og:description" content="Next generation healthcare software approach!" />
           <meta property="og:url" content="https://helpwave.de/" />
           <meta property="og:type" content="website" />
           <meta name="twitter:card" content="summary" />
+
         </Head>
         <body>
           <Main />

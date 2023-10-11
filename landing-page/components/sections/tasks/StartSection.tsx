ad098ec9ed4c267e790cbc1090714f57e442eaef
use-to <13922835+use-to@users.noreply.github.com>
2023-10-11 17:39:39 +0200

    feat: add tasks start section

--- landing-page/components/sections/tasks/StartSection.tsx
+++ landing-page/components/sections/tasks/StartSection.tsx
@@ -1,18 +1,19 @@
-import { tw } from '@helpwave/common/twind'
+import { tw, tx } from '@helpwave/common/twind'
+import Image from 'next/image'
 
 const StartSection = () => {
+  const screenshotURL = 'https://cdn.helpwave.de/screenshots/tasks_1.png'
+  const size = 1024
+
   return (
-    <div className={tw('pt-48 pb-16 flex gap-32 items-center justify-center')}>
-      <div className={tw('desktop:w-1/2')}>
-        <div className={tw('font-space text-6xl font-bold')}>helpwave <span className={tw('text-hw-primary-800')}>tasks</span></div>
+    <div className={tw('pt-48 pb-16')}>
+      <div className={tw('font-space text-6xl font-bold')}>helpwave <span className={tw('text-hw-primary-800')}>tasks</span></div>
 
-        <div className={tw('font-sans text-2xl font-medium mt-2')}>
-          The first open-source team management platform for healthcare workers
-        </div>
+      <div className={tw('font-sans text-2xl font-medium mt-2')}>
+        The first open-source team management platform for healthcare workers
       </div>
 
-      <div className={tw('mobile:hidden w-2/7')}>
-      </div>
+      <Image alt="Screenshots" src={screenshotURL} style={{ objectFit: 'contain' }} width={size} height={size} className={tx(`w-[${size}px] shadow-2xl w-full rounded-md mt-8`)}/>
     </div>
   )
 }
--- landing-page/pages/tasks/index.tsx
+++ landing-page/pages/tasks/index.tsx
@@ -8,7 +8,9 @@ const Tasks: NextPage = () => {
   return (
     <div className={tw('w-screen h-screen bg-white parent relative z-0 overflow-x-hidden')}>
       <Header />
-      <StartSection />
+      <div className={tw('desktop:w-5/12 desktop:mx-auto mobile:mx-8 relative z-[1]')}>
+        <StartSection />
+      </div>
       <Footer />
     </div>
   )

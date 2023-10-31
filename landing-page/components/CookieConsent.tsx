e07bfc2681634569472307f2d62fcf6d13af271d
Florian Sabonchi <54689374+florian-sabonchi@users.noreply.github.com>
2023-10-31 17:16:39 +0100

    feat: only load Google Analytics if accepted

--- landing-page/components/CookieConsent.tsx
+++ landing-page/components/CookieConsent.tsx
@@ -3,13 +3,11 @@
 import { useEffect } from 'react'
 import * as CookieConsent from 'vanilla-cookieconsent'
 import 'vanilla-cookieconsent/dist/cookieconsent.css'
-import getConfig from './CookieConsentConfig'
-import addEventListeners from './CookieConsentListeners'
+import pluginConfig from './CookieConsentConfig'
 
 const CookieConsentComponent = () => {
   useEffect(() => {
-    addEventListeners()
-    CookieConsent.run(getConfig())
+    CookieConsent.run(pluginConfig)
   }, [])
 
   return (
--- landing-page/pages/_app.tsx
+++ landing-page/pages/_app.tsx
@@ -31,18 +31,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           }
         `}</style>
       </Head>
-
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZQDJEWMMX6" />
-      <Script id="google-analytics">
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-ZQDJEWMMX6');
-      `}
-      </Script>
-
       <QueryClientProvider client={queryClient}>
         <ProvideLanguage>
           <div className={tw('font-sans')}>

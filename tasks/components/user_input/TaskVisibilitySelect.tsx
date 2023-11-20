23ec7fb4fce7f734a2b9d754e671bf18f099a1e0
PaulKalho <67231903+PaulKalho@users.noreply.github.com>
2023-11-20 14:45:42 +0100

    feat: dropdown for task visibility options on create
    * feat: dropdown for task visibility options on create
    
    * style: add props-type to TaskVisibilitySelect component
    
    * style: fix formatting

---
+++ tasks/components/user_input/TaskVisibilitySelect.tsx
@@ -0,0 +1,47 @@
+import { Select } from '@helpwave/common/components/user_input/Select'
+import type { SelectProps } from '@helpwave/common/components/user_input/Select'
+import type { PropsWithLanguage } from '@helpwave/common/hooks/useTranslation'
+import { useTranslation } from '@helpwave/common/hooks/useTranslation'
+
+type TaskVisibilitySelectTranslation = {
+  private: string,
+  public: string
+}
+
+const defaultTaskVisibilitySelectTranslation = {
+  en: {
+    private: 'private',
+    public: 'public',
+  },
+  de: {
+    private: 'privat',
+    public: 'öffentlich',
+  }
+}
+
+type TaskVisibilitySelectProps = Omit<SelectProps<boolean>, 'options'>
+
+/**
+ * A component for selecting a TaskVisibility
+ *
+ * The state is managed by the parent
+ */
+export const TaskVisibilitySelect = ({
+  language,
+  value,
+  ...selectProps
+}: PropsWithLanguage<TaskVisibilitySelectTranslation, TaskVisibilitySelectProps>) => {
+  const translation = useTranslation(language, defaultTaskVisibilitySelectTranslation)
+  const options = [
+    { value: true, label: translation.public },
+    { value: false, label: translation.private },
+  ]
+
+  return (
+    <Select
+      value={value}
+      options={options}
+      {...selectProps}
+    />
+  )
+}

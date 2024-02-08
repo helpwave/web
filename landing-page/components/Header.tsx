d54478731270af388d26d643e6563307c74185c7
Felix Thape <felix.thape@gmail.com>
2024-02-08 12:45:47 +0100

    feat: update header

--- landing-page/components/Header.tsx
+++ landing-page/components/Header.tsx
@@ -2,17 +2,47 @@ import { Span } from '@helpwave/common/components/Span'
 import { Helpwave } from '@helpwave/common/icons/Helpwave'
 import { tw } from '@helpwave/common/twind'
 import { Menu, X } from 'lucide-react'
-import Link from 'next/link'
 import { useState } from 'react'
+import Link from 'next/link'
+import { MenuItem } from '@helpwave/common/components/user-input/Menu'
+import { router } from 'next/client'
 
 const homeURL = '/'
 
-const items = [
-  { name: 'tasks', url: '/product/tasks' },
-  { name: 'story', url: '/story' },
-  { name: 'team', url: '/team' },
-  { name: 'talks', url: '/talks' },
-  { name: 'contact', url: 'mailto:contact@helpwave.de' },
+type LinkType = {
+  name: string,
+  url: string
+}
+
+type SubLinkType = LinkType & {
+  subpage?: LinkType[]
+}
+
+const items: SubLinkType[] = [
+  {
+    name: 'product',
+    url: '/product',
+    subpage: [{
+      name: 'tasks',
+      url: '/tasks',
+    }]
+  },
+  {
+    name: 'story',
+    url: '/story'
+  },
+  {
+    name: 'team',
+    url: '/team'
+  },
+  {
+    name: 'talks',
+    url: '/talks'
+  },
+  {
+    name: 'contact',
+    url: 'mailto:contact@helpwave.de'
+  },
 ]
 
 const Header = () => {
@@ -23,26 +53,47 @@ const Header = () => {
       <div className={tw('w-screen z-[50] fixed shadow-sm top-0 border bg-white')}>
         <nav className={tw('flex mobile:p-2 desktop:w-5/12 items-center justify-between mx-auto')}>
           <Link href={homeURL}>
-            <Helpwave />
+            <Helpwave/>
           </Link>
-
           <div className={tw('phone:hidden w-full')}>
             <div className={tw('flex flex-wrap items-center justify-evenly')}>
-              {items.map(({ name, url }) => (
+              {items.map(({
+                name,
+                url,
+                subpage
+              }) => (
                 <div key={name}>
-                  <Link href={url}>
-                    <Span type="navigationItem">
-                      {name}
-                    </Span>
-                  </Link>
+                  {subpage === undefined ? (
+                    <Link href={url}>
+                      <Span type="navigationItem">
+                        {name}
+                      </Span>
+                    </Link>
+                  ) : (
+                    <Menu name={name}>
+                      {subpage.map(({
+                        name: subPageName,
+                        url: subPageUrl
+                      }) =>
+                        (
+                          <MenuItem key={subPageName} onClick={() => router.push(url + subPageUrl)}>
+                            <Span type="navigationItem">
+                              {subPageName}
+                            </Span>
+                          </MenuItem>
+                        ))}
+                    </Menu>
+                  )}
                 </div>
               ))}
             </div>
           </div>
 
           <div className={tw('w-full tablet:hidden desktop:hidden flex justify-between mx-2')}>
-            <Link href={homeURL}><span className={tw('text-center text-2xl font-bold font-space')}>helpwave</span></Link>
-            <button onClick={() => setNavbarOpen(true)} className={tw('tablet:hidden desktop:hidden content-end')} aria-controls="navbar" aria-expanded="false">
+            <Link href={homeURL}><span
+              className={tw('text-center text-2xl font-bold font-space')}>helpwave</span></Link>
+            <button onClick={() => setNavbarOpen(true)} className={tw('tablet:hidden desktop:hidden content-end')}
+                    aria-controls="navbar" aria-expanded="false">
               <Menu size={32}/>
             </button>
           </div>
@@ -53,7 +104,7 @@ const Header = () => {
         <div className={tw('fixed w-screen h-screen z-[100] bg-white')}>
           <div className={tw('text-center content-center fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2')}>
             <button onClick={() => setNavbarOpen(false)} className={tw('mb-5')}>
-              <X size={64} />
+              <X size={64}/>
             </button>
 
             <div className={tw('w-full p-2')}>
@@ -64,7 +115,10 @@ const Header = () => {
               </Link>
             </div>
 
-            {items.map(({ name, url }) => (
+            {items.map(({
+              name,
+              url
+            }) => (
               <div key={name} className={tw('w-full p-2')}>
                 <Link href={url} onClick={() => setNavbarOpen(false)}>
                   <Span type="heading">

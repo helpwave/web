9d841d5dd256637e6d942e37657a1bdd2a731e38
Felix Thape <felix.thape@gmail.com>
2024-02-13 00:11:26 +0100

    feat: use requestAnimationFrame opposed to timeouts

--- lib/components/Ring.tsx
+++ lib/components/Ring.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { tw, tx } from '../twind'
 import { noop } from '../util/noop'
 import { Circle } from './Circle'
@@ -8,7 +8,7 @@ export type RingProps = {
   width: number,
   color?: string, // Tailwind color
   className?: string
-}
+};
 
 export const Ring = ({
   innerSize = 20,
@@ -18,7 +18,10 @@ export const Ring = ({
 }: RingProps) => {
   return (
     <div
-      className={tx(`w-[${innerSize}px] h-[${innerSize}px] bg-transparent rounded-full outline outline-[${width}px] outline-${color}`, className)}
+      className={tx(
+        `w-[${innerSize}px] h-[${innerSize}px] bg-transparent rounded-full outline outline-[${width}px] outline-${color}`,
+        className
+      )}
     />
   )
 }
@@ -27,7 +30,7 @@ export type AnimatedRingProps = RingProps & {
   fillAnimationDuration?: number, // in seconds, 0 means no animation
   repeating?: boolean,
   onAnimationFinished?: () => void
-}
+};
 
 export const AnimatedRing = ({
   innerSize,
@@ -40,31 +43,46 @@ export const AnimatedRing = ({
 }: AnimatedRingProps) => {
   const [currentWidth, setCurrentWidth] = useState(0)
   const milliseconds = 1000 * fillAnimationDuration
-  const updatesPerSecond = 30
+
+  const animate = useCallback((timestamp: number, startTime: number) => {
+    const progress = Math.min((timestamp - startTime) / milliseconds, 1)
+    const newWidth = Math.min(width * progress, width)
+
+    setCurrentWidth(newWidth)
+
+    if (progress < 1) {
+      requestAnimationFrame((newTimestamp) => animate(newTimestamp, startTime))
+    } else {
+      onAnimationFinished()
+      if (repeating) {
+        setCurrentWidth(0)
+        requestAnimationFrame((newTimestamp) =>
+          animate(newTimestamp, newTimestamp)
+        )
+      }
+    }
+  }, [milliseconds, onAnimationFinished, repeating, width])
 
   useEffect(() => {
     if (currentWidth < width) {
-      const intervalId = setInterval(() => {
-        setCurrentWidth((prevWidth) => {
-          const newWidth = Math.min(prevWidth + (width / (fillAnimationDuration * updatesPerSecond)), width)
-          if (newWidth >= width) {
-            onAnimationFinished()
-            if (repeating) {
-              return 0
-            }
-          }
-          return newWidth
-        })
-      }, 1000 / updatesPerSecond) // Adjust the interval as needed
-
-      return () => clearInterval(intervalId)
+      requestAnimationFrame((timestamp) => animate(timestamp, timestamp))
     }
-  }, [width, milliseconds, currentWidth, fillAnimationDuration, repeating, onAnimationFinished])
+  }, [])
 
   return (
     <div
-      className={tx(`w-[${innerSize + 2 * width}px] h-[${innerSize + 2 * width}px] flex flex-row items-center justify-center`)}>
-      <Ring innerSize={innerSize} width={currentWidth} color={color} className={className}/>
+      className={tx(
+        `w-[${innerSize + 2 * width}px] h-[${
+          innerSize + 2 * width
+        }px] flex flex-row items-center justify-center`
+      )}
+    >
+      <Ring
+        innerSize={innerSize}
+        width={currentWidth}
+        color={color}
+        className={className}
+      />
     </div>
   )
 }
@@ -72,7 +90,7 @@ export const AnimatedRing = ({
 export type RingWaveProps = Omit<AnimatedRingProps, 'innerSize'> & {
   startInnerSize: number,
   endInnerSize: number
-}
+};
 
 export const RingWave = ({
   startInnerSize = 20,
@@ -87,36 +105,54 @@ export const RingWave = ({
   const [currentInnerSize, setCurrentInnerSize] = useState(startInnerSize)
   const distance = endInnerSize - startInnerSize
   const milliseconds = 1000 * fillAnimationDuration
-  const updatesPerSecond = 30
+
+  const animate = useCallback((timestamp: number, startTime: number) => {
+    const progress = Math.min((timestamp - startTime) / milliseconds, 1)
+    const newInnerSize = Math.min(
+      startInnerSize + distance * progress,
+      endInnerSize
+    )
+
+    setCurrentInnerSize(newInnerSize)
+
+    if (progress < 1) {
+      requestAnimationFrame((newTimestamp) => animate(newTimestamp, startTime))
+    } else {
+      onAnimationFinished()
+      if (repeating) {
+        setCurrentInnerSize(startInnerSize)
+        requestAnimationFrame((newTimestamp) =>
+          animate(newTimestamp, newTimestamp)
+        )
+      }
+    }
+  }, [distance, endInnerSize, milliseconds, onAnimationFinished, repeating, startInnerSize])
 
   useEffect(() => {
     if (currentInnerSize < endInnerSize) {
-      const intervalId = setInterval(() => {
-        setCurrentInnerSize((prevInnerSize) => {
-          const newInnerSize = Math.min(prevInnerSize + (distance / (fillAnimationDuration * updatesPerSecond)), endInnerSize)
-          if (newInnerSize >= endInnerSize) {
-            onAnimationFinished()
-            if (repeating) {
-              return startInnerSize
-            }
-          }
-          return newInnerSize
-        })
-      }, 1000 / updatesPerSecond) // Adjust the interval as needed
-
-      return () => clearInterval(intervalId)
+      requestAnimationFrame((timestamp) => animate(timestamp, timestamp))
     }
-  }, [milliseconds, currentInnerSize, fillAnimationDuration, repeating, onAnimationFinished, endInnerSize, distance, startInnerSize])
+  }, [])
 
   return (
     <div
-      className={tx(`w-[${endInnerSize + 2 * width}px] h-[${endInnerSize + 2 * width}px] flex flex-row items-center justify-center`)}>
-      <Ring innerSize={currentInnerSize} width={width} color={color} className={className}/>
+      className={tx(
+        `w-[${endInnerSize + 2 * width}px] h-[${
+          endInnerSize + 2 * width
+        }px] flex flex-row items-center justify-center`
+      )}
+    >
+      <Ring
+        innerSize={currentInnerSize}
+        width={width}
+        color={color}
+        className={className}
+      />
     </div>
   )
 }
 
-export type RadialRingsProps = NonNullable<unknown>
+export type RadialRingsProps = NonNullable<unknown>;
 
 // eslint-disable-next-line no-empty-pattern
 export const RadialRings = ({}: RadialRingsProps) => {
@@ -128,19 +164,29 @@ export const RadialRings = ({}: RadialRingsProps) => {
       <Circle
         radius={50}
         color="hw-primary-700"
-        className={tw(`absolute z-[10] left-[${size / 2}px] top-[${size / 2}px] -translate-y-1/2 -translate-x-1/2`)}
+        className={tw(
+          `absolute z-[10] left-[${size / 2}px] top-[${
+            size / 2
+          }px] -translate-y-1/2 -translate-x-1/2`
+        )}
       />
       {currentRing === 0 ? (
         <AnimatedRing
           innerSize={100}
           width={50}
           color="hw-primary-500"
-          onAnimationFinished={() => currentRing === 0 ? setCurrentRing(1) : null}
+          onAnimationFinished={() =>
+            currentRing === 0 ? setCurrentRing(1) : null
+          }
           repeating={true}
-          className={tx({ 'opacity-5': currentRing !== 0 }, `absolute z-[9] left-[${size / 2}px] top-[${size / 2}px] -translate-y-1/2 -translate-x-1/2`)}
+          className={tx(
+            { 'opacity-5': currentRing !== 0 },
+            `absolute z-[9] left-[${size / 2}px] top-[${
+              size / 2
+            }px] -translate-y-1/2 -translate-x-1/2`
+          )}
         />
-      ) : null
-      }
+      ) : null}
       {currentRing === 2 ? (
         <RingWave
           startInnerSize={100 - 10}
@@ -148,26 +194,39 @@ export const RadialRings = ({}: RadialRingsProps) => {
           width={10}
           color="white"
           repeating={true}
-          className={tx(`absolute z-[9] left-[${size / 2}px] top-[${size / 2}px] -translate-y-1/2 -translate-x-1/2 opacity-5`)}
+          className={tx(
+            `absolute z-[9] left-[${size / 2}px] top-[${
+              size / 2
+            }px] -translate-y-1/2 -translate-x-1/2 opacity-5`
+          )}
         />
-      ) : null
-      }
+      ) : null}
       <Circle
         radius={100}
         color="hw-primary-500"
-        className={tx({ 'opacity-20': currentRing < 1 }, `absolute z-[8] left-[${size / 2}px] top-[${size / 2}px] -translate-y-1/2 -translate-x-1/2`)}
+        className={tx(
+          { 'opacity-20': currentRing < 1 },
+          `absolute z-[8] left-[${size / 2}px] top-[${
+            size / 2
+          }px] -translate-y-1/2 -translate-x-1/2`
+        )}
       />
       {currentRing === 1 ? (
         <AnimatedRing
           innerSize={199}
           width={50}
           color="hw-primary-400"
-          onAnimationFinished={() => currentRing === 1 ? setCurrentRing(2) : null}
+          onAnimationFinished={() =>
+            currentRing === 1 ? setCurrentRing(2) : null
+          }
           repeating={true}
-          className={tx(`absolute z-[7] left-[${size / 2}px] top-[${size / 2}px] -translate-y-1/2 -translate-x-1/2`)}
+          className={tx(
+            `absolute z-[7] left-[${size / 2}px] top-[${
+              size / 2
+            }px] -translate-y-1/2 -translate-x-1/2`
+          )}
         />
-      ) : null
-      }
+      ) : null}
       {currentRing === 2 ? (
         <RingWave
           startInnerSize={200}
@@ -175,14 +234,22 @@ export const RadialRings = ({}: RadialRingsProps) => {
           width={10}
           color="white"
           repeating={true}
-          className={tx(`absolute z-[7] left-[${size / 2}px] top-[${size / 2}px] -translate-y-1/2 -translate-x-1/2 opacity-5`)}
+          className={tx(
+            `absolute z-[7] left-[${size / 2}px] top-[${
+              size / 2
+            }px] -translate-y-1/2 -translate-x-1/2 opacity-5`
+          )}
         />
-      ) : null
-      }
+      ) : null}
       <Circle
         radius={150}
         color="hw-primary-400"
-        className={tx({ 'opacity-20': currentRing < 2 }, `absolute z-[6] left-[${size / 2}px] top-[${size / 2}px] -translate-y-1/2 -translate-x-1/2`)}
+        className={tx(
+          { 'opacity-20': currentRing < 2 },
+          `absolute z-[6] left-[${size / 2}px] top-[${
+            size / 2
+          }px] -translate-y-1/2 -translate-x-1/2`
+        )}
       />
     </div>
   )

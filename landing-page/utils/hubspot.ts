2faf1d6ed232ff40198285b19fb1563f2b2e4d1d
Max Schäfer <40192999+MaxSchaefer@users.noreply.github.com>
2024-02-17 11:24:54 +0100

    feat: connect NewsletterForm with HubSpot (#874)
    * feat: connect NewsletterForm with HubSpot
    
    * fix: remove public routing of the newsletter page

--- landing-page/utils/hubspot.ts
+++ landing-page/utils/hubspot.ts
@@ -1,20 +1,19 @@
-const HUBSPOT_PORTAL_ID = '26536657'
-const HUBSPOT_FORM_ID = 'e5271b1a-1ab8-472b-aae1-d4e3ab52fab7'
+export type FormField<T = string|number> = {
+  objectTypeId: string,
+  name: string,
+  value: T
+}
 
-export const hubspotSubmitForm = ({ firstName, lastName, email, message }: { firstName: string, lastName: string, email: string, message: string }) =>
-  fetch(`https://api.hsforms.com/submissions/v3/integration/submit/${HUBSPOT_PORTAL_ID}/${HUBSPOT_FORM_ID}`, {
+export const submitHubSpotForm = (portalId: string, formId: string, formFields: FormField[]) => {
+  return fetch(`https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formId}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       submittedAt: Date.now(),
-      fields: [
-        { objectTypeId: '0-1', name: 'firstname', value: firstName },
-        { objectTypeId: '0-1', name: 'lastname', value: lastName },
-        { objectTypeId: '0-1', name: 'email', value: email },
-        { objectTypeId: '0-1', name: 'message', value: message }
-      ],
+      fields: formFields,
+
       // TODO: context attribute?
       legalConsentOptions: { // TODO: 🤡
         consent: {
@@ -30,4 +29,5 @@ export const hubspotSubmitForm = ({ firstName, lastName, email, message }: { fir
         }
       }
     })
-  }).then(res => res.json())
+  })
+}
---
+++ lib/components/LoadingButton.tsx
@@ -0,0 +1,24 @@
+import { tx } from '../twind'
+import { Helpwave } from '../icons/Helpwave'
+import type { ButtonProps } from './Button'
+import { Button } from './Button'
+
+type LoadingButtonProps = {
+  isLoading?: boolean
+} & ButtonProps
+
+export const LoadingButton = ({ isLoading = false, ...rest }: LoadingButtonProps) => {
+  return (
+    <div className={tx('inline-block relative')}>
+      {
+        isLoading && (
+          <div className={tx('absolute inset-0 flex justify-center items-center bg-white/50')}>
+            <div className={tx('bg-white opacity-50')}/>
+            <Helpwave animate="loading"/>
+          </div>
+        )
+      }
+      <Button {...rest} disabled={isLoading}/>
+    </div>
+  )
+}

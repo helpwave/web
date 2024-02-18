34c8462330f1ae6c714f251d8c5c17e484405ef7
Max Schäfer <max-schaefer-lg@gmx.de>
2024-02-18 23:59:30 +0100

    fix: linter

--- landing-page/components/sections/landing/Story.tsx
+++ landing-page/components/sections/landing/Story.tsx
@@ -31,24 +31,24 @@ const StorySection = () => {
       <StoryBlock
         pill="Open Innovation"
         header="Providing healthcare solutions"
-        content={"At helpwave, we\'re not just creating healthcare software; we\'re co-creating it with you, the"
-          + " end-users. By directly involving you in the process, we\'re elevating quality and nurturing stronger"
-          + " development relationships. This unique approach allows our dedicated developers and engineers to laser-focus"
-          + " on turning your requirements into innovative software features."}
+        content={`At helpwave, we're not just creating healthcare software; we're co-creating it with you, the
+          end-users. By directly involving you in the process, we're elevating quality and nurturing stronger
+          development relationships. This unique approach allows our dedicated developers and engineers to laser-focus
+          on turning your requirements into innovative software features.`}
       />
       <StoryBlock
         pill="Disruption"
         header="Access for everyone"
-        content={"When did you last feel like the software you\'re using is worth billions? We didn\'t think so."
-          + " Regulatory overhead and high entry barriers are making it hard for small companies to enter the market which leads to a lack of competition."
-          + " helpwave is here to change that. We are providing a platform that invites everyone to the table, not just only the big players."}
+        content={`When did you last feel like the software you're using is worth billions? We didn't think so.
+          Regulatory overhead and high entry barriers are making it hard for small companies to enter the market which leads to a lack of competition.
+          helpwave is here to change that. We are providing a platform that invites everyone to the table, not just only the big players.`}
       />
       <StoryBlock
         pill="Our mission"
-        header={"Passion for our vision"}
-        content={"Our commitment to maintaining the highest level of agility, much like what helpwave embodies, is driving"
-          + " unprecedented innovation across medical branches. Experience a healthcare software development like never"
-          + " before and join us on this transformative journey"}
+        header="Passion for our vision"
+        content={`Our commitment to maintaining the highest level of agility, much like what helpwave embodies, is driving
+          unprecedented innovation across medical branches. Experience a healthcare software development like never
+          before and join us on this transformative journey`}
       />
     </div>
   )

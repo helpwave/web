4ed5a93c2f384f1a7f2bb49721f5aa9a636841d4
Felix <67233923+DasProffi@users.noreply.github.com>
2024-02-18 13:59:07 +0100

    feat: product call to action button (#872)
    * feat: add manage properties button
    
    * feat: add call to action to main page
    
    * feat: change padding
    
    Co-authored-by: Felix <13922835+use-to@users.noreply.github.com>
    
    * feat: update design for mobile
    
    * feat: update button position
    
    ---------
    
    Co-authored-by: Felix <13922835+use-to@users.noreply.github.com>

--- landing-page/pages/index.tsx
+++ landing-page/pages/index.tsx
@@ -1,5 +1,8 @@
 import { tw } from '@helpwave/common/twind'
 import type { NextPage } from 'next'
+import { DescriptionWithAction } from '@helpwave/common/components/DescriptionWithAction'
+import Link from 'next/link'
+import { MousePointerClick } from 'lucide-react'
 import Divider from '../components/Divider'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
@@ -11,24 +14,44 @@ import StorySection from '../components/sections/landing/Story'
 const Home: NextPage = () => {
   return (
     <div className={tw('w-screen h-screen bg-white parent relative z-0 overflow-x-hidden')}>
-      <Header />
+      <Header/>
       <div className={tw('desktop:w-5/12 desktop:mx-auto mobile:mx-8 relative z-[1]')}>
-        <StartSection />
+        <StartSection/>
       </div>
-      <Divider rotate={1} />
+      <Divider rotate={1}/>
       <div className={tw('desktop:w-5/12 desktop:mx-auto tablet:mx-16 phone:mx-8 relative z-[1]')}>
-        <PartnerSection />
+        <PartnerSection/>
       </div>
-      <Divider rotate={1} />
-      <div className={tw('desktop:w-5/12 desktop:mx-auto mobile:mx-8 relative z-[1]')}>
-        <StorySection />
+      <Divider rotate={1}/>
+      <div className={tw('desktop:w-5/12 desktop:mx-auto mobile:mx-8 relative z-[1] desktop:pb-8 mobile:pb-16')}>
+        <StorySection/>
       </div>
-      <div className={tw('w-screen parent bg-hw-primary-900')}>
-        <div className={tw('desktop:w-5/12 desktop:mx-auto mobile:mx-8 pt-16 text-white')}>
-          <ExpansionSection />
+      <div className={tw('w-screen parent bg-hw-primary-900 relative')}>
+        <div className={tw('desktop:w-5/12 desktop:mx-auto mobile:mx-8 desktop:pt-24 mobile:pt-32 text-white')}>
+          <ExpansionSection/>
+        </div>
+        <div className={tw('absolute z-[2] desktop:w-[620px] top-0 left-1/2 -translate-x-1/2 -translate-y-1/2')}>
+          <DescriptionWithAction
+            // TODO translation
+            title="Check out our first product..."
+            description="...and how helpwave tasks increases productivity for our medical heroes"
+            trailing={(
+              <div className={tw('flex flex-row items-center desktop:justify-end mobile:justify-center grow desktop:pl-2 mobile:pt-2')}>
+                <Link
+                  href="product/tasks"
+                  className={tw('flex flex-row gap-x-4 whitespace-nowrap px-4 py-2 text-white bg-hw-primary-800 rounded-md')}
+                >
+                  helpwave tasks
+                  <MousePointerClick/>
+                </Link>
+              </div>
+            )}
+            className={tw('!bg-gray-200 !py-4 !px-8 shadow')}
+            descriptionClassName={tw('!text-gray-600')}
+          />
         </div>
       </div>
-      <Footer />
+      <Footer/>
     </div>
   )
 }

9b1cc3a57046dd3e1f3c715a0a94e02cb437d63c
Felix Thape <felix.thape@gmail.com>
2024-03-01 23:25:36 +0100

    feat: add ScrollPicker.tsx

--- lib/util/array.ts
+++ lib/util/array.ts
@@ -22,3 +22,37 @@ export const closestMatch = <T, >(list: T[], firstCloser: (item1: T, item2: T) =
     return firstCloser(item1, item2) ? item1 : item2
   })
 }
+
+/**
+ * returns the item in middle of a list and its neighbours before and after
+ * e.g. [1,2,3,4,5,6] for item = 1 would return [5,6,1,2,3]
+ */
+export const getNeighbours = <T, >(list: T[], item: T, neighbourDistance: number = 2) => {
+  const index = list.indexOf(item)
+  const totalItems = neighbourDistance * 2 + 1
+  if (list.length < totalItems) {
+    console.warn('List is to short')
+    return list
+  }
+
+  if (index === -1) {
+    console.error('item not found in list')
+    return list.splice(0, totalItems)
+  }
+
+  let start = index - neighbourDistance
+  if (start < 0) {
+    start += list.length
+  }
+  const end = (index + neighbourDistance + 1) % list.length
+
+  const result:T[] = []
+  let ignoreOnce = list.length === totalItems
+  for (let i = start; i !== end || ignoreOnce; i = (i + 1) % list.length) {
+    result.push(list[i]!)
+    if (end === i && ignoreOnce) {
+      ignoreOnce = false
+    }
+  }
+  return result
+}

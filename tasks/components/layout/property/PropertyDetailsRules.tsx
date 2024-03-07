c721695bff9b3d065b7df31b213dba86f057cee4
Felix Thape <felix.thape@gmail.com>
2024-03-08 00:32:56 +0100

    chore: soft-required with checkbox

--- tasks/components/layout/property/PropertyDetailsRules.tsx
+++ tasks/components/layout/property/PropertyDetailsRules.tsx
@@ -2,26 +2,24 @@ import type { Languages } from '@helpwave/common/hooks/useLanguage'
 import type { PropsWithLanguage } from '@helpwave/common/hooks/useTranslation'
 import { useTranslation } from '@helpwave/common/hooks/useTranslation'
 import { InputGroup } from '@helpwave/common/components/InputGroup'
-import { Select } from '@helpwave/common/components/user-input/Select'
 import { Tile } from '@helpwave/common/components/layout/Tile'
 import { Checkbox } from '@helpwave/common/components/user-input/Checkbox'
 import { tw } from '@helpwave/common/twind'
-import type { PropertyRules } from '@/components/layout/property/property'
-import { importanceList } from '@/components/layout/property/property'
+import type { ImportanceType, PropertyRules } from '@/components/layout/property/property'
 
 type PropertyDetailsRulesTranslation = {
   rules: string,
   importance: string,
-  optional: string,
-  softRequired: string,
+  importanceDescription: string,
   alwaysVisible: string,
   alwaysVisibleDescription: string
-}
+} & {[key in ImportanceType]: string}
 
 const defaultPropertyDetailsRulesTranslation: Record<Languages, PropertyDetailsRulesTranslation> = {
   en: {
     rules: 'Rules',
     importance: 'Importance',
+    importanceDescription: 'The property gets visually highlighted on an entity if it is empty.',
     optional: 'Optional',
     softRequired: 'Soft-Required',
     alwaysVisible: 'Always Visible',
@@ -30,6 +28,7 @@ const defaultPropertyDetailsRulesTranslation: Record<Languages, PropertyDetailsR
   de: {
     rules: 'Regeln',
     importance: 'Wichtigkeit',
+    importanceDescription: 'Die Eigenschaft wird auf einer Entität visuell hervorgehoben wenn sie nicht ausgefüllt ist.',
     optional: 'Optional',
     softRequired: 'Soft-Required', // TODO better translation
     alwaysVisible: 'Immer sichtbar',
@@ -53,12 +52,16 @@ export const PropertyDetailsRules = ({
   const translation = useTranslation(language, defaultPropertyDetailsRulesTranslation)
   return (
     <InputGroup title={translation.rules}>
-      <Select
-        // TODO add icons
-        value={value.importance}
-        label={{ name: translation.importance, labelType: 'labelMedium' }}
-        options={importanceList.map(importance => ({ value: importance, label: translation[importance] }))}
-        onChange={importance => onChange({ ...value, importance })}
+      <Tile
+        title={{ value: translation.softRequired, type: 'labelMedium' }}
+        description={{ value: translation.importanceDescription }}
+        suffix={(
+          <Checkbox
+            checked={value.importance === 'softRequired'}
+            onChange={isAlwaysVisible => onChange({ ...value, importance: isAlwaysVisible ? 'softRequired' : 'optional' })}
+            size={20}
+          />
+        )}
       />
       <Tile
         title={{ value: translation.alwaysVisible, type: 'labelMedium' }}

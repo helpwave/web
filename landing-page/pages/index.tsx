0ce8ea23b32e6b32db9c1271684c63c115b86b1d
Felix <67233923+DasProffi@users.noreply.github.com>
2024-03-27 00:24:51 +0100

    fix: fix padding around product banner (#944)
    * fix: fix padding around product banner
    
    * fix: fix small gap

--- landing-page/pages/index.tsx
+++ landing-page/pages/index.tsx
@@ -20,7 +20,7 @@ type HomePageTranslation = {
   helpwaveTasks: string
 }
 
-const defaultHomePageTranslation : Record<Languages, HomePageTranslation> = {
+const defaultHomePageTranslation: Record<Languages, HomePageTranslation> = {
   en: {
     checkOutFirstProduct: 'Check out our first product',
     checkOutFirstProductDescription: 'and how helpwave tasks increases productivity for our medical heroes',
@@ -46,20 +46,18 @@ const Home: NextPage = ({ overwriteTranslation }: PropsForTranslation<HomePageTr
         <PartnerSection/>
       </div>
       <Divider rotate={1}/>
-      <div className={tw('desktop:w-7/12 desktop:mx-auto mobile:mx-8 relative z-[1] desktop:pb-8 mobile:pb-16')}>
+      <div className={tw('desktop:w-7/12 desktop:mx-auto mobile:mx-8')}>
         <StorySection/>
       </div>
-      <div className={tw('w-screen parent bg-hw-primary-900 relative')}>
-        <div className={tw('desktop:w-5/12 desktop:mx-auto mobile:mx-8 desktop:pt-24 mobile:pt-32 text-white')}>
-          <ExpansionSection/>
-        </div>
-        <div className={tw('absolute z-[2] desktop:w-[620px] top-0 left-1/2 -translate-x-1/2 -translate-y-1/2')}>
+      <div className={tw('relative flex flex-col items-center')}>
+        <div className={tw('desktop:w-[620px] mx-20')}>
           <DescriptionWithAction
             // TODO translation
             title={`${translation.checkOutFirstProduct}...`}
             description={`...${translation.checkOutFirstProductDescription}`}
             trailing={(
-              <div className={tw('flex flex-row items-center desktop:justify-end mobile:justify-center grow desktop:pl-2 mobile:pt-2')}>
+              <div
+                className={tw('flex flex-row items-center desktop:justify-end mobile:justify-center grow desktop:pl-2 mobile:pt-2')}>
                 <Link
                   href="product/tasks"
                   className={tw('flex flex-row gap-x-4 whitespace-nowrap px-4 py-2 text-white bg-hw-primary-800 rounded-md')}
@@ -73,6 +71,10 @@ const Home: NextPage = ({ overwriteTranslation }: PropsForTranslation<HomePageTr
             descriptionClassName={tw('!text-gray-600')}
           />
         </div>
+        <div className={tw('absolute h-1/2 w-full top-1/2 bg-hw-primary-900 z-[-1] translate-y-[2px]')}></div>
+      </div>
+      <div className={tw('w-screen parent bg-hw-primary-900 text-white')}>
+        <ExpansionSection/>
       </div>
       <Footer/>
     </div>

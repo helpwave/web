d4399cbb82118af6a660c485b4dbde9089e492eb
Felix Thape <felix.thape@gmail.com>
2024-03-27 15:34:51 +0100

    chore: update property tailwind use

--- lib/components/properties/PropertyBase.tsx
+++ lib/components/properties/PropertyBase.tsx
@@ -47,26 +47,21 @@ export const PropertyBase = ({
   const translation = useTranslation(defaultPropertyBaseTranslation, overwriteTranslation)
   const requiredAndNoValue = softRequired && !hasValue
   return (
-    <div
-      className={tx('flex flex-row border border-2 rounded-[16px]', {
-        'hover:border-hw-primary-800 border-gray-400 ': !requiredAndNoValue,
-        'hover:border-hw-warn-800 border-hw-warn-700': requiredAndNoValue,
-      }, className)}
-    >
+    <div className={tx('flex flex-row group overflow-hidden', className)}>
       <div
         className={
-          tx('flex flex-row gap-x-2 min-w-[200px] overflow-hidden px-4 py-2 items-center rounded-l-[14px]', {
-            'bg-gray-200 text-black': !requiredAndNoValue,
-            'bg-hw-warn-600 text-hw-warn-100': requiredAndNoValue,
+          tx('flex flex-row gap-x-2 min-w-[200px] overflow-hidden px-4 py-2 items-center rounded-l-xl border-2 border-r-0', {
+            'bg-gray-200 text-black group-hover:border-hw-primary-800 border-gray-400': !requiredAndNoValue,
+            'bg-hw-warn-600 text-hw-warn-100 group-hover:border-hw-warn-800 border-hw-warn-700': requiredAndNoValue,
           }, className)}
       >
         {icon}
         {name}
       </div>
       <div className={
-        tx('flex grow justify-between items-center rounded-r-[14px]', {
-          'bg-white': !requiredAndNoValue,
-          'bg-hw-warn-200': requiredAndNoValue,
+        tx('flex grow justify-between items-center rounded-r-xl border-2 border-l-0', {
+          'bg-white group-hover:border-hw-primary-800 border-gray-400': !requiredAndNoValue,
+          'bg-hw-warn-200 group-hover:border-hw-warn-800 border-hw-warn-700': requiredAndNoValue,
         }, className)}
       >
         {input({ softRequired, hasValue })}

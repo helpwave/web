7fc00ca14d586168bf363f6e2fab2259f7b1fff9
Felix Thape <felix.thape@gmail.com>
2024-04-05 22:51:13 +0200

    feat: update team section with new profile images

--- landing-page/pages/team/index.tsx
+++ landing-page/pages/team/index.tsx
@@ -8,7 +8,7 @@ const Team: NextPage = () => {
   return (
     <div className={tw('w-screen h-screen bg-white parent relative z-0 overflow-x-hidden')}>
       <Header />
-      <div className={tw('desktop:w-5/12 desktop:mx-auto mobile:mx-8 relative z-[1]')}>
+      <div className={tw('desktop:max-w-[1200px] desktop:mx-auto desktop:px-16 mobile:mx-8 relative z-[1]')}>
         <TeamSection />
       </div>
       <Footer />

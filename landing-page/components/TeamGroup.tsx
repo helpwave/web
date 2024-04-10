5eb836402dd1411f9f97b263d698ecf64c9e3c3c
Max Schäfer <40192999+MaxSchaefer@users.noreply.github.com>
2024-04-10 23:23:05 +0200

    Update landing-page/components/TeamGroup.tsx

--- landing-page/components/TeamGroup.tsx
+++ landing-page/components/TeamGroup.tsx
@@ -15,7 +15,7 @@ const TeamGroup = ({
     <div className={tw('mb-8 flex flex-wrap text-center justify-center gap-8')}>
       <h2 className={tw('w-full text-6xl underline my-8')}>{name}</h2>
       {members.map(member => (
-        <Profile key={member.name} {...member} className={tw('!bg-gray-100 border-2 border-black')}/>
+        <Profile key={member.name} {...member}/>
       ))}
     </div>
   )

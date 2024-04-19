d0f81e8190f38dbc556643aef5e2d57daf004f17
Felix Thape <felix.thape@gmail.com>
2024-04-19 14:29:13 +0200

    feat: merge main and update example image

--- lib/stories/layout/TextImage.stories.tsx
+++ lib/stories/layout/TextImage.stories.tsx
@@ -17,7 +17,7 @@ export const TextImageVariation: Story = {
       'gravida facilisis. Phasellus volutpat elit luctus, sagittis libero sit amet, dapibus lacus.',
     color: 'primary',
     badge: 'Step #1',
-    imageUrl: 'https://source.boringavatars.com/marble/2048/Maria?square=true',
+    imageUrl: 'https://images.unsplash.com/photo-1576091160550-2173dba999ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     imageClassName: '',
     contentClassName: '',
     className: '',

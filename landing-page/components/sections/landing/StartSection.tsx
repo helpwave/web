f482cc7a0f086af20ffb88c2060221e2b0661701
Felix Thape <felix.thape@gmail.com>
2024-05-24 12:42:08 +0200

    feat: update image

--- landing-page/components/sections/landing/StartSection.tsx
+++ landing-page/components/sections/landing/StartSection.tsx
@@ -15,11 +15,11 @@ type LandingPageTranslation = {
 const defaultLandingPageTranslation: Record<Languages, LandingPageTranslation> = {
   en: {
     title: 'helpwave - the Operating System for Hospitals',
-    description: 'empowering medical heroes, united in technology'
+    description: 'empowering \\primary{medical heroes}, united in \\positive{technology}'
   },
   de: {
     title: 'helpwave - das Betriebssystem für Krankenhäuser',
-    description: 'Stärkung medizinischer Helden, vereint in Technologie'
+    description: 'Stärkung \\primary{medizinischer Helden}, vereint in \\positive{Technologie}'
   }
 }
 
@@ -28,19 +28,19 @@ const StartSection = ({ overwriteTranslation }: PropsForTranslation<LandingPageT
   return (
     <SectionBase className={tw('flex flex-row mobile:!flex-wrap-reverse w-full !max-w-full gap-8 justify-between mobile:justify-end items-center !pr-0')}>
       <div className={tw('flex flex-col items-center flex-1 mobile:pr-6')}>
-        <div className={tw('flex flex-col gap-y-2 max-w-[500px]')}>
+        <div className={tw('flex flex-col gap-y-2 max-w-[600px]')}>
           <h1><Span type="title" className={tw('!text-4xl')}>{translation.title}</Span></h1>
-          <Span className={tw('font-space font-semibold')}><MarkdownInterpreter text={translation.description}/></Span>
+          <Span className={tw('font-space font-semibold !text-2xl')}><MarkdownInterpreter text={translation.description}/></Span>
         </div>
       </div>
-      <div className={tw('relative right-0 p-4 rounded-l-3xl bg-white w-2/5 tablet:min-w-[360px] mobile:w-4/5 z-10 h-fit shadow-xl')}>
+      <div className={tw('relative right-0 p-4 pr-0 rounded-l-3xl bg-white w-2/5 tablet:min-w-[360px] mobile:w-4/5 z-10 h-fit shadow-xl')}>
         <Image
-          // TODO replace image
-          src="https://cdn.helpwave.de/screenshots/tasks_2.png"
+          // TODO make attribution to https://www.freepik.com/free-vector/medics-working-charts_4950249.htm
+          src="https://cdn.helpwave.de/landing_page/doctor_statistics.svg"
           alt=""
           width={0}
           height={0}
-          className={tw('w-full')}
+          className={tw('w-full rounded-l-lg')}
         />
       </div>
     </SectionBase>

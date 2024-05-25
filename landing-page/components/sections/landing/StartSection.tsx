518b4ae9172dd44bf22af090a27284a01df27854
Felix Thape <felix.thape@gmail.com>
2024-05-25 12:49:29 +0200

    feat: update image and layout

--- landing-page/components/sections/landing/StartSection.tsx
+++ landing-page/components/sections/landing/StartSection.tsx
@@ -26,14 +26,14 @@ const defaultLandingPageTranslation: Record<Languages, LandingPageTranslation> =
 const StartSection = ({ overwriteTranslation }: PropsForTranslation<LandingPageTranslation>) => {
   const translation = useTranslation(defaultLandingPageTranslation, overwriteTranslation)
   return (
-    <SectionBase className={tw('flex flex-row mobile:!flex-wrap-reverse w-full !max-w-full gap-8 justify-between mobile:justify-end items-center !pr-0')}>
-      <div className={tw('flex flex-col items-center flex-1 mobile:pr-6')}>
+    <SectionBase className={tw('flex flex-row mobile:!flex-wrap-reverse w-full !max-w-full gap-8 justify-between mobile:justify-center items-center desktop:pr-0 tablet:pr-0')}>
+      <div className={tw('flex flex-col items-center flex-1')}>
         <div className={tw('flex flex-col gap-y-2 max-w-[600px]')}>
           <h1><Span type="title" className={tw('!text-4xl')}>{translation.title}</Span></h1>
           <Span className={tw('font-space font-semibold !text-2xl')}><MarkdownInterpreter text={translation.description}/></Span>
         </div>
       </div>
-      <div className={tw('relative right-0 p-4 pr-0 rounded-l-3xl bg-white w-2/5 tablet:min-w-[360px] mobile:w-4/5 z-10 h-fit shadow-xl')}>
+      <div className={tw('desktop:relative desktop:right-0 p-4 pr-0 mobile:pr-4 rounded-l-3xl mobile:rounded-3xl bg-white w-2/5 tablet:min-w-[360px] mobile:w-4/5 z-10 h-fit shadow-xl')}>
         <Image
           // TODO make attribution to https://www.freepik.com/free-vector/medics-working-charts_4950249.htm
           src="https://cdn.helpwave.de/landing_page/doctor_statistics.svg"

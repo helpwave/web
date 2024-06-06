690e6fd81eae0e446796328b9c07a6dd8cb81e1d
Max Schäfer <40192999+MaxSchaefer@users.noreply.github.com>
2024-06-06 11:28:53 +0200

    fix: remove StoryInUseAt component from Story page (#1072)

--- landing-page/pages/story/index.tsx
+++ landing-page/pages/story/index.tsx
@@ -2,14 +2,12 @@ import type { NextPage } from 'next'
 import StoryHeader from '@/components/sections/story/StoryHeader'
 import { Page } from '@/components/Page'
 import { StorySliderSection } from '@/components/sections/landing/StoriesSliderSection'
-import StoryInUseAtSection from '@/components/sections/story/StoryInUseAtSection'
 
 const Story: NextPage = () => {
   return (
     <Page>
       <StoryHeader/>
       <StorySliderSection/>
-      <StoryInUseAtSection/>
     </Page>
   )
 }

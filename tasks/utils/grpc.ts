4a0940bf3c86a15da759be4622c0afb0d4c1dd7d
Max Schäfer <max-schaefer-lg@gmx.de>
2024-06-20 14:33:29 +0200

    chore: update @helpwave/proto-ts and removing /proto/ prefix

--- tasks/utils/grpc.ts
+++ tasks/utils/grpc.ts
@@ -1,16 +1,16 @@
-import { WardServicePromiseClient } from '@helpwave/proto-ts/proto/services/task_svc/v1/ward_svc_grpc_web_pb'
-import { RoomServicePromiseClient } from '@helpwave/proto-ts/proto/services/task_svc/v1/room_svc_grpc_web_pb'
-import { BedServicePromiseClient } from '@helpwave/proto-ts/proto/services/task_svc/v1/bed_svc_grpc_web_pb'
-import { PatientServicePromiseClient } from '@helpwave/proto-ts/proto/services/task_svc/v1/patient_svc_grpc_web_pb'
+import { WardServicePromiseClient } from '@helpwave/proto-ts/services/task_svc/v1/ward_svc_grpc_web_pb'
+import { RoomServicePromiseClient } from '@helpwave/proto-ts/services/task_svc/v1/room_svc_grpc_web_pb'
+import { BedServicePromiseClient } from '@helpwave/proto-ts/services/task_svc/v1/bed_svc_grpc_web_pb'
+import { PatientServicePromiseClient } from '@helpwave/proto-ts/services/task_svc/v1/patient_svc_grpc_web_pb'
 import Cookies from 'js-cookie'
 import {
   TaskTemplateServicePromiseClient
-} from '@helpwave/proto-ts/proto/services/task_svc/v1/task_template_svc_grpc_web_pb'
-import { TaskServicePromiseClient } from '@helpwave/proto-ts/proto/services/task_svc/v1/task_svc_grpc_web_pb'
+} from '@helpwave/proto-ts/services/task_svc/v1/task_template_svc_grpc_web_pb'
+import { TaskServicePromiseClient } from '@helpwave/proto-ts/services/task_svc/v1/task_svc_grpc_web_pb'
 import {
   OrganizationServicePromiseClient
-} from '@helpwave/proto-ts/proto/services/user_svc/v1/organization_svc_grpc_web_pb'
-import { UserServicePromiseClient } from '@helpwave/proto-ts/proto/services/user_svc/v1/user_svc_grpc_web_pb'
+} from '@helpwave/proto-ts/services/user_svc/v1/organization_svc_grpc_web_pb'
+import { UserServicePromiseClient } from '@helpwave/proto-ts/services/user_svc/v1/user_svc_grpc_web_pb'
 import { COOKIE_ID_TOKEN_KEY } from '../hooks/useAuth'
 import { getConfig } from './config'
 import { LOCALSTORAGE_ORGANIZATION_KEY } from '@/hooks/useOrganization'

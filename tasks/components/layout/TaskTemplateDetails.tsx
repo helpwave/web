d588c1557459eb6e40b8aa4d1cb3eb3084025b9b
Batkhongor Chagnaa <ch.bathongor@gmail.com>
2024-11-12 16:37:11 +0100

    fix(issue#1108): create button is centered within the content of new task creation

--- tasks/components/layout/TaskTemplateDetails.tsx
+++ tasks/components/layout/TaskTemplateDetails.tsx
@@ -1,69 +1,80 @@
-import { useContext, useState } from 'react'
-import { tw, tx } from '@helpwave/common/twind'
-import type { Languages } from '@helpwave/common/hooks/useLanguage'
-import { useTranslation, type PropsForTranslation } from '@helpwave/common/hooks/useTranslation'
-import { Button } from '@helpwave/common/components/Button'
-import { ConfirmDialog } from '@helpwave/common/components/modals/ConfirmDialog'
-import { Input } from '@helpwave/common/components/user-input/Input'
-import { Span } from '@helpwave/common/components/Span'
-import { Textarea } from '@helpwave/common/components/user-input/Textarea'
-import type { TaskTemplateDTO } from '@helpwave/api-services/types/tasks/tasks_templates'
-import { SubtaskView } from '../SubtaskView'
-import { ColumnTitle } from '../ColumnTitle'
-import { TaskTemplateContext, type TaskTemplateFormType } from '@/pages/templates'
+import { useContext, useState } from "react";
+import { tw, tx } from "@helpwave/common/twind";
+import type { Languages } from "@helpwave/common/hooks/useLanguage";
+import {
+  useTranslation,
+  type PropsForTranslation,
+} from "@helpwave/common/hooks/useTranslation";
+import { Button } from "@helpwave/common/components/Button";
+import { ConfirmDialog } from "@helpwave/common/components/modals/ConfirmDialog";
+import { Input } from "@helpwave/common/components/user-input/Input";
+import { Span } from "@helpwave/common/components/Span";
+import { Textarea } from "@helpwave/common/components/user-input/Textarea";
+import type { TaskTemplateDTO } from "@helpwave/api-services/types/tasks/tasks_templates";
+import { SubtaskView } from "../SubtaskView";
+import { ColumnTitle } from "../ColumnTitle";
+import {
+  TaskTemplateContext,
+  type TaskTemplateFormType,
+} from "@/pages/templates";
 
 type TaskTemplateDetailsTranslation = {
-  updateTaskTemplate: string,
-  updateTaskTemplateDescription: string,
-  createTaskTemplate: string,
-  name: string,
-  notes: string,
-  deleteTaskTemplate: string,
-  deleteConfirmText: string,
-  create: string,
-  update: string,
-  tooLong: (maxCharacters: number) => string,
-  tooShort: (minCharacters: number) => string,
-  required: string
-}
+  updateTaskTemplate: string;
+  updateTaskTemplateDescription: string;
+  createTaskTemplate: string;
+  name: string;
+  notes: string;
+  deleteTaskTemplate: string;
+  deleteConfirmText: string;
+  create: string;
+  update: string;
+  tooLong: (maxCharacters: number) => string;
+  tooShort: (minCharacters: number) => string;
+  required: string;
+};
 
-const defaultTaskTemplateDetailsTranslations: Record<Languages, TaskTemplateDetailsTranslation> = {
+const defaultTaskTemplateDetailsTranslations: Record<
+  Languages,
+  TaskTemplateDetailsTranslation
+> = {
   en: {
-    updateTaskTemplate: 'Update Task Template',
-    updateTaskTemplateDescription: 'Here you can update details about the Task Template',
-    createTaskTemplate: 'Create Task Template',
-    name: 'Name',
-    notes: 'Notes',
-    deleteTaskTemplate: 'Delete Task Template',
-    deleteConfirmText: 'Do you really want to delete this templates?',
-    create: 'Create',
-    update: 'Update',
+    updateTaskTemplate: "Update Task Template",
+    updateTaskTemplateDescription:
+      "Here you can update details about the Task Template",
+    createTaskTemplate: "Create Task Template",
+    name: "Name",
+    notes: "Notes",
+    deleteTaskTemplate: "Delete Task Template",
+    deleteConfirmText: "Do you really want to delete this templates?",
+    create: "Create",
+    update: "Update",
     tooLong: (maxCharacters) => `Too long, at most ${maxCharacters} characters`,
-    tooShort: (minCharacters) => `Too short, at least ${minCharacters} characters`,
-    required: 'Required Field, cannot be empty'
+    tooShort: (minCharacters) =>
+      `Too short, at least ${minCharacters} characters`,
+    required: "Required Field, cannot be empty",
   },
   de: {
-    updateTaskTemplate: 'Task Template bearbeiten',
-    updateTaskTemplateDescription: 'Hier kannst du deine Vorlagen bearbeiten',
-    createTaskTemplate: 'Task Template erstellen',
-    name: 'Name',
-    notes: 'Notizen',
-    deleteTaskTemplate: 'Task Template löschen',
-    deleteConfirmText: 'Wollen Sie wirklich dieses Task Template löschen?',
-    create: 'Erstellen',
-    update: 'Ändern',
+    updateTaskTemplate: "Task Template bearbeiten",
+    updateTaskTemplateDescription: "Hier kannst du deine Vorlagen bearbeiten",
+    createTaskTemplate: "Task Template erstellen",
+    name: "Name",
+    notes: "Notizen",
+    deleteTaskTemplate: "Task Template löschen",
+    deleteConfirmText: "Wollen Sie wirklich dieses Task Template löschen?",
+    create: "Erstellen",
+    update: "Ändern",
     tooLong: (maxCharacters) => `Zu lang, maximal ${maxCharacters} Zeichen`,
     tooShort: (minCharacters) => `Zu kurz, mindestens ${minCharacters} Zeichen`,
-    required: 'Benötigter Wert, darf nicht leer sein'
-  }
-}
+    required: "Benötigter Wert, darf nicht leer sein",
+  },
+};
 
 export type TaskTemplateDetailsProps = {
-  onCreate: (taskTemplate: TaskTemplateDTO) => void,
-  onUpdate: (taskTemplate: TaskTemplateFormType) => void,
-  onDelete: (taskTemplate: TaskTemplateDTO) => void,
-  width?: number
-}
+  onCreate: (taskTemplate: TaskTemplateDTO) => void;
+  onUpdate: (taskTemplate: TaskTemplateFormType) => void;
+  onDelete: (taskTemplate: TaskTemplateDTO) => void;
+  width?: number;
+};
 
 /**
  * The right side of the task templates page
@@ -73,38 +84,51 @@ export const TaskTemplateDetails = ({
   onCreate,
   onUpdate,
   onDelete,
-}: PropsForTranslation<TaskTemplateDetailsTranslation, TaskTemplateDetailsProps>) => {
-  const context = useContext(TaskTemplateContext)
+}: PropsForTranslation<
+  TaskTemplateDetailsTranslation,
+  TaskTemplateDetailsProps
+>) => {
+  const context = useContext(TaskTemplateContext);
 
-  const translation = useTranslation(defaultTaskTemplateDetailsTranslations, overwriteTranslation)
-  const isCreatingNewTemplate = context.state.template.id === ''
-  const [isShowingConfirmDialog, setIsShowingConfirmDialog] = useState(false)
+  const translation = useTranslation(
+    defaultTaskTemplateDetailsTranslations,
+    overwriteTranslation
+  );
+  const isCreatingNewTemplate = context.state.template.id === "";
+  const [isShowingConfirmDialog, setIsShowingConfirmDialog] = useState(false);
   const [touched, setTouched] = useState({
-    name: !isCreatingNewTemplate
-  })
+    name: !isCreatingNewTemplate,
+  });
 
-  const inputErrorClasses = tw('border-hw-negative-500 focus:border-hw-negative-500 focus:ring-hw-negative-500 border-2')
-  const inputClasses = tw('mt-1 block rounded-md w-full border-gray-300 shadow-sm focus:outline-none focus:border-hw-primary-500 focus:ring-hw-primary-500')
+  const inputErrorClasses = tw(
+    "border-hw-negative-500 focus:border-hw-negative-500 focus:ring-hw-negative-500 border-2"
+  );
+  const inputClasses = tw(
+    "mt-1 block rounded-md w-full border-gray-300 shadow-sm focus:outline-none focus:border-hw-primary-500 focus:ring-hw-primary-500"
+  );
 
-  const minNameLength = 2
-  const maxNameLength = 32
+  const minNameLength = 2;
+  const maxNameLength = 32;
 
   function validateName(name: string) {
-    const taskTemplateName = name.trim()
-    if (taskTemplateName === '') {
-      return translation.required
+    const taskTemplateName = name.trim();
+    if (taskTemplateName === "") {
+      return translation.required;
     } else if (taskTemplateName.length < minNameLength) {
-      return translation.tooShort(minNameLength)
+      return translation.tooShort(minNameLength);
     } else if (taskTemplateName.length > maxNameLength) {
-      return translation.tooLong(maxNameLength)
+      return translation.tooLong(maxNameLength);
     }
   }
 
-  const nameErrorMessage: string | undefined = validateName(context.state.template.name)
-  const isDisplayingNameError: boolean = touched.name && nameErrorMessage !== undefined
+  const nameErrorMessage: string | undefined = validateName(
+    context.state.template.name
+  );
+  const isDisplayingNameError: boolean =
+    touched.name && nameErrorMessage !== undefined;
 
   return (
-    <div className={tw('flex flex-col py-4 px-6')}>
+    <div className={tw("flex flex-col py-4 px-6")}>
       <ConfirmDialog
         id="TaskTemplateDetails-DeleteDialog"
         titleText={translation.deleteConfirmText}
@@ -113,16 +137,24 @@ export const TaskTemplateDetails = ({
         onBackgroundClick={() => setIsShowingConfirmDialog(false)}
         onCloseClick={() => setIsShowingConfirmDialog(false)}
         onConfirm={() => {
-          setIsShowingConfirmDialog(false)
-          onDelete(context.state.template)
+          setIsShowingConfirmDialog(false);
+          onDelete(context.state.template);
         }}
         confirmType="negative"
       />
       <ColumnTitle
-        title={isCreatingNewTemplate ? translation.createTaskTemplate : translation.updateTaskTemplate}
-        subtitle={!isCreatingNewTemplate ? translation.updateTaskTemplateDescription : undefined}
+        title={
+          isCreatingNewTemplate
+            ? translation.createTaskTemplate
+            : translation.updateTaskTemplate
+        }
+        subtitle={
+          !isCreatingNewTemplate
+            ? translation.updateTaskTemplateDescription
+            : undefined
+        }
       />
-      <div className={tw(' flex flex-col gap-y-4 max-w-[400px] mb-4')}>
+      <div className={tw(" flex flex-col gap-y-4 max-w-[400px] mb-4")}>
         <div>
           <Input
             id="name"
@@ -130,60 +162,76 @@ export const TaskTemplateDetails = ({
             label={{ name: translation.name }}
             type="text"
             onBlur={() => setTouched({ name: true })}
-            onChange={text => {
+            onChange={(text) => {
               context.updateContext({
                 template: { ...context.state.template, name: text },
                 isValid: validateName(text) === undefined,
                 hasChanges: true,
-                deletedSubtaskIds: context.state.deletedSubtaskIds
-              })
+                deletedSubtaskIds: context.state.deletedSubtaskIds,
+              });
             }}
             maxLength={maxNameLength}
-            className={tx(inputClasses, { [inputErrorClasses]: isDisplayingNameError })}
+            className={tx(inputClasses, {
+              [inputErrorClasses]: isDisplayingNameError,
+            })}
           />
-          {isDisplayingNameError && <Span type="formError">{nameErrorMessage}</Span>}
+          {isDisplayingNameError && (
+            <Span type="formError">{nameErrorMessage}</Span>
+          )}
         </div>
         <Textarea
           headline={translation.notes}
           id="notes"
           value={context.state.template.notes}
-          onChange={text => {
+          onChange={(text) => {
             context.updateContext({
               template: { ...context.state.template, notes: text },
               isValid: context.state.isValid,
               hasChanges: true,
-              deletedSubtaskIds: context.state.deletedSubtaskIds
-            })
+              deletedSubtaskIds: context.state.deletedSubtaskIds,
+            });
           }}
         />
       </div>
       <SubtaskView
         taskTemplateId={context.state.template.id}
         subtasks={context.state.template.subtasks}
-        onChange={subtasks => context.updateContext({
-          hasChanges: true,
-          isValid: context.state.isValid,
-          template: { ...context.state.template, subtasks },
-          deletedSubtaskIds: context.state.deletedSubtaskIds
-        })}
+        onChange={(subtasks) =>
+          context.updateContext({
+            hasChanges: true,
+            isValid: context.state.isValid,
+            template: { ...context.state.template, subtasks },
+            deletedSubtaskIds: context.state.deletedSubtaskIds,
+          })
+        }
       />
-      <div className={tx('flex flex-row mt-12',
-        {
-          'justify-between': !isCreatingNewTemplate,
-          'justify-end': isCreatingNewTemplate,
+      <div
+        className={tx("flex flex-row mt-12", {
+          "justify-between": !isCreatingNewTemplate,
+          "justify-center": isCreatingNewTemplate,
         })}
       >
         <Button
-          className={tw('w-auto')}
-          onClick={() => isCreatingNewTemplate ? onCreate(context.state.template) : onUpdate(context.state)}
+          className={tw("w-auto")}
+          onClick={() =>
+            isCreatingNewTemplate
+              ? onCreate(context.state.template)
+              : onUpdate(context.state)
+          }
           disabled={!context.state.isValid}
         >
           {isCreatingNewTemplate ? translation.create : translation.update}
         </Button>
-        { !isCreatingNewTemplate &&
-          (<Button variant="textButton" color="negative" onClick={() => setIsShowingConfirmDialog(true)}>{translation.deleteTaskTemplate}</Button>)
-        }
+        {!isCreatingNewTemplate && (
+          <Button
+            variant="textButton"
+            color="negative"
+            onClick={() => setIsShowingConfirmDialog(true)}
+          >
+            {translation.deleteTaskTemplate}
+          </Button>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};

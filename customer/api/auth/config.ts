029e1451dd2ec0032b2d9aace0729b0a75730778
Felix Evers <git@felixevers.de>
2025-03-10 15:44:49 +0100

    feat: update redirect urls

--- customer/api/auth/config.ts
+++ customer/api/auth/config.ts
@@ -1,5 +1,5 @@
 /// TODO change and move to env
 export const OIDC_PROVIDER = 'https://id.helpwave.de/realms/main/'
 export const CLIENT_ID = 'customer-api'
-export const REDIRECT_URI = 'http://localhost:3000/'
-export const POST_LOGOUT_REDIRECT_URI = 'http://localhost:3000/'
+export const REDIRECT_URI = 'https://customer.helpwave.de'
+export const POST_LOGOUT_REDIRECT_URI = 'https://customer.helpwave.de/'

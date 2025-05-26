4377f96a3742f82d3fb3920e12eea35966249464
Felix <67233923+DasProffi@users.noreply.github.com>
2025-05-26 16:34:44 +0200

    chore: create default organization when none is available (#1221)
    * fix: fix header design
    
    * chore: add organization service
    
    * chore: create new organization when none exists with fake token
    
    * chore: remove organisation id from ward queries
    
    * fix: fix lint

--- tasks/components/Header.tsx
+++ tasks/components/Header.tsx
@@ -1,12 +1,14 @@
 import type { ReactNode } from 'react'
 import Link from 'next/link'
-
 import { Helpwave } from '@helpwave/hightide'
+import clsx from 'clsx'
 
 export type HeaderProps = {
   title?: string,
   leftSide?: ReactNode[],
+  leftSideClassName?: string,
   rightSide?: ReactNode[],
+  rightSideClassName?: string,
   /**
    * @default true
    */
@@ -22,29 +24,36 @@ export type HeaderProps = {
  *
  * each element in left and right is also seperated by the divider
  */
-const Header = ({ title, leftSide = [], rightSide = [], withIcon = true }: HeaderProps) => {
+const Header = ({
+                  title,
+                  leftSide = [],
+                  leftSideClassName,
+                  rightSide = [],
+                  rightSideClassName,
+                  withIcon = true
+                }: HeaderProps) => {
   return (
-    <div className="row h-16 py-4 relative items-center justify-between border-b border-slate-900/10">
-      <div className="row gap-4 w-full justify-between mx-4">
-        <div className="row w-full relative items-center align-center">
+    <div className="row h-16 py-4 relative items-center justify-between border-b border-divider">
+      <div className="row w-full gap-x-8 justify-between mx-4">
+        <div className="row relative items-center align-center">
           {withIcon && (
             <div className="row relative align-center">
               <Link href="/">
-                <Helpwave size={52} />
+                <Helpwave size={52}/>
               </Link>
             </div>
           )}
           {title && <span className="textstyle-title-md">{title}</span>}
           {leftSide?.filter(value => value !== undefined).map((value, index) => (
-            <div key={'leftAction' + index} className="row items-center">
+            <div key={'leftAction' + index} className={clsx('row items-center', leftSideClassName)}>
               {(index !== 0 || title || withIcon) && <div className="bg-gray-300 rounded h-8 w-0.5 mx-4"/>}
               {value}
             </div>
           ))}
         </div>
-        <div className="row w-full items-center justify-end">
+        <div className="row items-center justify-end">
           {rightSide?.filter(value => value !== undefined).map((value, index) => (
-            <div key={'rightAction' + index} className="row items-center">
+            <div key={'rightAction' + index} className={clsx('row items-center', rightSideClassName)}>
               {value}
               {index !== rightSide?.length - 1 && <div className="bg-gray-300 rounded h-8 w-0.5 mx-3"/>}
             </div>
